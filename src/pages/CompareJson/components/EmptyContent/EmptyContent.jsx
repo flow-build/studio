df0bf399f968d20c6dfad2ea585b10683df0973e
Elson Oliveira <elson.oliveira@fdte.io>
2022-03-17 09:56:10 -0300

    Empty compare created

---
+++ src/pages/CompareJson/components/EmptyContent/EmptyContent.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+import Container from "@mui/material/Container";
+import ArticleIcon from "@mui/icons-material/Article";
+import Typography from "@mui/material/Typography";
+
+const EmptyContent = () => {
+  const containerStyleSX = {
+    height: "100%",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+  };
+
+  const articleIconStyleSX = { fontSize: 70, marginBottom: "30px" };
+
+  return (
+    <Container fixed sx={containerStyleSX}>
+      <ArticleIcon sx={articleIconStyleSX} />
+
+      <Typography variant="h4" component="h4">
+        Nenhum objeto selecionado
+      </Typography>
+      <Typography variant="h4" component="h4">
+        para a comparação
+      </Typography>
+    </Container>
+  );
+};
+
+export default EmptyContent;
