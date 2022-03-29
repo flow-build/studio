ae63bceb40ec3c696d2f91c3e7fffcac093e42c7
Elson Oliveira <elson.oliveira@fdte.io>
2022-03-29 10:26:08 -0300

    Comparing JSON side by side

--- src/pages/CompareJson/CompareJson.jsx
+++ src/pages/CompareJson/CompareJson.jsx
@@ -1,97 +1,414 @@
-import React, { useMemo } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
 
-import _isUndefined from "lodash/isUndefined";
+import _isEqual from "lodash/isEqual";
+import _isEmpty from "lodash/isEmpty";
 
+import { styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import Paper from "@mui/material/Paper";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
 
-import { setNotification } from "features/notificationsSlice";
+import "./teste.css";
+import EmptyContent from "./components/EmptyContent/EmptyContent";
 
-import { useCompareJson } from "pages/CompareJson/hooks/useCompareJson";
-import EmptyContent from "pages/CompareJson/components/EmptyContent/EmptyContent";
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+  ...theme.typography.body2,
+  textAlign: "center",
+  color: theme.palette.text.secondary,
+}));
 
-const CompareJson = () => {
-  const dispatch = useDispatch();
-  const { onCompare } = useCompareJson();
+const isArray = (item) => {
+  if (item === "array") {
+    return true;
+  }
+  return Object.prototype.toString.call(item) === "[object Array]";
+};
 
-  const compare = useSelector((state) => state.compare);
+const isObject = (item) => {
+  return Object.prototype.toString.call(item) === "[object Object]";
+};
 
-  const compareDataTeste = useMemo(() => {
-    if (_isUndefined(compare?.oldJson) || _isUndefined(compare?.newJson)) {
-      return;
+const needFormat = (type) => {
+  return type === "array" || type === "object";
+};
+
+const getIndent = (level) => {
+  if (level === 1) {
+    return { textIndent: "20px" };
+  }
+  return { textIndent: `${level * 20}px` };
+};
+
+const getType = (item) => {
+  let t = Object.prototype.toString.call(item);
+  let match = /(?!\[).+(?=\])/g;
+  t = t.match(match)[0].split(" ")[1];
+  return t.toLowerCase();
+};
+
+const isComplexType = (param) => {
+  return isObject(param) || isArray(param);
+};
+
+const isTheSametype = (a, b) => {
+  return (
+    Object.prototype.toString.call(a) === Object.prototype.toString.call(b)
+  );
+};
+
+const mergeData = (_old, _new, type) => {
+  // finally result
+  let result = [];
+  // each line No.
+  let lineLevel = 1;
+
+  // convert array or object to Array<object> [{}]
+  const convertObject = (param, lineType) => {
+    let list = [];
+    if (isComplexType(param)) {
+      let showIndex = getType(param) === "object";
+      let keys = Object.keys(param);
+      let length = keys.length;
+      keys.forEach((key, index) => {
+        let type = getType(param[key]);
+        list.push({
+          name: key,
+          line: lineLevel++,
+          value: convertObject(param[key], lineType),
+          type: type,
+          showIndex: showIndex,
+          needComma: length !== index + 1,
+          lineType: lineType,
+          lastLineType: lineType,
+          lastLine: isComplexType(param[key]) ? lineLevel++ : null,
+        });
+      });
+      return list;
+    } else {
+      switch (getType(param)) {
+        case "number":
+        case "boolean":
+        case "regexp":
+          return param.toString();
+        case "null":
+          return "null";
+        case "undefined":
+          return "undefined";
+        case "function":
+          return " ƒ() {...}";
+        default:
+          return `"${param.toString()}"`;
+      }
+    }
+  };
+
+  // return parsed data
+  const parseValue = (key, value, showIndex, needComma, lineType) => {
+    return {
+      name: key,
+      line: lineLevel++,
+      value: convertObject(value, lineType),
+      type: getType(value),
+      showIndex: showIndex,
+      needComma: needComma,
+      lineType: lineType,
+      lastLineType: lineType,
+      lastLine: isComplexType(value) ? lineLevel++ : null,
+    };
+  };
+
+  const findRemovedKeys = (from = [], to = []) => {
+    return from.filter((fromElem) => !to.some((toElem) => toElem === fromElem));
+  };
+
+  // merge two vars to target,target type Array<object>[{}]
+  const parseData = (oldJson, newJson, target) => {
+    let oldJsonKeys = Object.keys(oldJson);
+    let newJsonKeys = Object.keys(newJson);
+    let showIndex = isObject(newJson);
+
+    // deleted keys
+    let deletedKeys = findRemovedKeys(oldJsonKeys, newJsonKeys);
+
+    // not removed keys
+    let keptKeys = oldJsonKeys.filter((ak) =>
+      newJsonKeys.some((bk) => bk === ak)
+    );
+
+    // new added keys
+    let addedKeys = findRemovedKeys(newJsonKeys, oldJsonKeys);
+
+    // console.log({ deletedKeys });
+    // console.log({ addedKeys });
+
+    if (type === "del") {
+      // push deleted keys
+      deletedKeys.forEach((key, index) => {
+        // console.log("delete array", deletedKeys);
+        let needComma = true;
+        if (
+          keptKeys.length === 0 &&
+          addedKeys.length === 0 &&
+          index === deletedKeys.length - 1
+        ) {
+          needComma = false;
+        }
+        target.push(parseValue(key, oldJson[key], showIndex, needComma, "del"));
+      });
     }
 
-    const { oldJson, newJson } = compare;
+    // The core function: compare
+    keptKeys.forEach((key, index) => {
+      let needComma = true;
+      if (addedKeys.length === 0 && index === keptKeys.length - 1) {
+        needComma = false;
+      }
 
-    const { data, isSuccess } = onCompare(oldJson, newJson);
+      if (_isEqual(oldJson[key], newJson[key])) {
+        // console.log("isEqual");
+        // console.log({ key });
+        target.push(
+          parseValue(key, newJson[key], showIndex, needComma, "none")
+        );
+      } else if (isTheSametype(oldJson[key], newJson[key])) {
+        // console.log("isTheSameType");
+        // console.log({ key });
+        if (isComplexType(newJson[key])) {
+          let _target = parseValue(
+            key,
+            isArray(oldJson[key]) ? [] : {},
+            showIndex,
+            needComma,
+            "none"
+          );
+          target.push(_target);
+          // back one step
+          lineLevel -= 1;
+          // go inside
+          parseData(oldJson[key], newJson[key], _target.value);
+          // rewrite lastline
+          _target.lastLine = lineLevel++;
+        } else {
+          if (type === "del") {
+            target.push(parseValue(key, oldJson[key], showIndex, true, "none"));
+          }
 
-    if (!isSuccess) {
-      const notification = {
-        type: "snackbar",
-        variant: "error",
-        message: data.message,
-      };
+          if (type === "add") {
+            target.push(
+              parseValue(key, newJson[key], showIndex, needComma, "none")
+            );
+          }
+        }
+      } else {
+        // console.log("else");
+        // console.log({ key });
+        if (type === "del") {
+          target.push(parseValue(key, oldJson[key], showIndex, true, "del"));
+        }
 
-      dispatch(setNotification(notification));
+        if (type === "add") {
+          target.push(
+            parseValue(key, newJson[key], showIndex, needComma, "add")
+          );
+        }
+      }
+    });
 
-      return;
+    if (type === "add") {
+      // push new keys
+      addedKeys.forEach((key, index) => {
+        target.push(
+          parseValue(
+            key,
+            newJson[key],
+            showIndex,
+            addedKeys.length !== index + 1,
+            "add"
+          )
+        );
+      });
     }
 
-    const { message, ...comparedValues } = data;
+    if (type === "del" && _isEmpty(deletedKeys) && !_isEmpty(addedKeys)) {
+      console.log("del");
+      target.push(parseValue("", "", showIndex, false, "empty"));
+    }
 
-    return comparedValues;
-  }, [dispatch, onCompare, compare]);
+    if (type === "add" && _isEmpty(addedKeys) && !_isEmpty(deletedKeys)) {
+      console.log("add");
+      target.push(parseValue("", "", showIndex, false, "empty"));
+    }
+  };
 
-  if (_isUndefined(compareDataTeste)) {
-    return <EmptyContent />;
+  if (isTheSametype(_old, _new) && isComplexType(_new)) {
+    parseData(_old, _new, result);
+  } else {
+    if (_old === _new) {
+      result.push(parseValue(0, _new, false, false, "none"));
+    } else {
+      if (type === "del") {
+        result.push(parseValue(0, _old, false, true, "del"));
+      }
+
+      if (type === "add") {
+        result.push(parseValue(1, _new, false, false, "add"));
+      }
+    }
+  }
+  return result;
+};
+
+const ComplexTree = (props) => {
+  let {
+    name,
+    value,
+    type,
+    line,
+    showIndex,
+    needComma,
+    level = 1,
+    lineType,
+    lastLineType,
+    lastLine = null,
+  } = props;
+
+  let [visiable, setVisiable] = useState(true);
+
+  if (lineType === "empty") {
+    return (
+      <p className={`c-json-p`} style={getIndent(level)}>
+        <span className="c-json-mark">{line}</span>
+        <span className={`c-of-${lineType}`}></span>
+        <span className="c-json-content">&nbsp;</span>
+      </p>
+    );
   }
 
   return (
-    <Grid container spacing={2} gridTemplateRows="1fr 0.5fr 1fr">
-      <Grid item xs={12} flex={1}>
-        <strong>Propriedades removidas:</strong>{" "}
-        {compareDataTeste.removedElements}
-        <br />
-        <strong>Propriedades adicionadas:</strong>{" "}
-        {compareDataTeste.addedElements}
-        <br />
-        <br />
-        <strong>Tipos de valores alterados:</strong>
-        <br />
-        <TableContainer component={Paper}>
-          <Table size="small" aria-label="a dense table">
-            <TableHead>
-              <TableRow>
-                <TableCell>Propriedade</TableCell>
-                <TableCell align="right">Antigo JSON</TableCell>
-                <TableCell align="right">Novo JSON</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {compareDataTeste.differentValues.map((row, index) => (
-                <TableRow
-                  key={index.toString()}
-                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-                >
-                  <TableCell component="th" scope="row">
-                    {row.prop}
-                  </TableCell>
-                  <TableCell align="right">{row.json1}</TableCell>
-                  <TableCell align="right">{row.json2}</TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
+    <div className="c-json-line">
+      <p
+        className={`c-json-p c-line-${lineType}`}
+        onClick={() => setVisiable(!visiable)}
+        style={getIndent(level)}
+      >
+        <span className="c-json-mark">{line}</span>
+        <span className={`c-of-${lineType}`}></span>
+        <span className="c-json-content">
+          {showIndex && <span className="c-json-key">{name}: </span>}
+          <span className="c-json-pt">{isArray(type) ? "[" : "{"}</span>
+        </span>
+        {!visiable && (
+          <span className="c-json-pt">
+            {isArray(type) ? "...]" : "...}"}
+            {needComma ? "," : ""}
+          </span>
+        )}
+      </p>
+      <div style={{ display: visiable ? "block" : "none" }}>
+        {value.map((item, index) => (
+          <Tree key={index} level={level + 1} {...item} />
+        ))}
+
+        <p
+          className={`c-json-feet c-json-p c-line-${lineType}`}
+          style={getIndent(level)}
+        >
+          {lastLine && <span className="c-json-mark">{lastLine}</span>}
+          {lastLineType && <span className={`c-of-${lastLineType}`}></span>}
+          <span className="c-json-pt">
+            {isArray(type) ? "]" : "}"}
+            {needComma ? "," : ""}
+          </span>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+function NormalTree(props) {
+  let {
+    name,
+    value,
+    line,
+    showIndex,
+    type,
+    lineType,
+    needComma,
+    level = 1,
+  } = props;
+
+  if (lineType === "empty") {
+    return (
+      <p className={`c-json-p`} style={getIndent(level)}>
+        <span className="c-json-mark">{line}</span>
+        <span className={`c-of-${lineType}`}></span>
+        <span className="c-json-content">&nbsp;</span>
+      </p>
+    );
+  }
+
+  return (
+    <p className={`c-json-p c-line-${lineType}`} style={getIndent(level)}>
+      <span className="c-json-mark">{line}</span>
+      <span className={`c-of-${lineType}`}></span>
+      <span className="c-json-content">
+        {showIndex && <span className="c-json-key">{name}: </span>}
+        <span className={`c-json-${type}`}>{value}</span>
+        <span className="c-json-comma">{needComma ? "," : ""}</span>
+      </span>
+    </p>
+  );
+}
+
+function Tree(props) {
+  let { type } = props;
+
+  if (needFormat(type)) {
+    return <ComplexTree {...props} />;
+  }
+  return <NormalTree {...props} />;
+}
+
+const CompareJson = () => {
+  const compare = useSelector((state) => state.compare);
+
+  if (_isEmpty(compare?.oldJson) || _isEmpty(compare?.newJson)) {
+    return <EmptyContent />;
+  }
+
+  const dataOld = mergeData(
+    JSON.parse(compare?.oldJson),
+    JSON.parse(compare?.newJson),
+    "del"
+  );
+  const dataNew = mergeData(
+    JSON.parse(compare?.oldJson),
+    JSON.parse(compare?.newJson),
+    "add"
+  );
+
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid container columns={12}>
+        <Grid item xs={6}>
+          <Item>
+            {dataOld.map((item, index) => (
+              <Tree key={index} {...item} />
+            ))}
+          </Item>
+        </Grid>
+
+        <Grid item xs={6}>
+          <Item>
+            {dataNew.map((item, index) => (
+              <Tree key={index} {...item} />
+            ))}
+          </Item>
+        </Grid>
       </Grid>
-    </Grid>
+    </Box>
   );
 };
 
--- src/pages/CompareJson/styles.js
+++ src/pages/CompareJson/styles.js
@@ -1,27 +1,9 @@
 import styled from "styled-components";
 
-import { Grid as MuiGrid } from "@mui/material";
-
-export const Grid = styled(MuiGrid).attrs({
-  height: "100%",
-  container: true,
-  // spacing: 1,
-})`
-  overflow: hidden;
-`;
-
-export const Item = styled(MuiGrid).attrs({
-  item: true,
-  xs: 6,
-})`
-  height: 100%;
-  overflow-y: scroll;
-  overflow-x: hidden;
-`;
-
-export const BigItem = styled(MuiGrid).attrs({
-  item: true,
-  xs: 12,
-})`
-  height: 50px;
+export const CJsonLine = styled.div`
+  position: relative;
+  text-align: left;
+  padding: 0;
+  margin: 0;
+  padding: 2px 0;
 `;
