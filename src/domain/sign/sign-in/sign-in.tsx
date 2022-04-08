974a0dd2cf3fd5957a5d1232b863a01494c8fb45
Elson Oliveira <elson.oliveira@fdte.io>
2022-04-08 13:13:13 -0300

    DDD sign-in

---
+++ src/domain/sign/sign-in/sign-in.tsx
@@ -0,0 +1,54 @@
+import { useCallback, useState } from 'react';
+
+import flowbuildLogo from 'assets/images/flowbuild-studio/default.svg'
+
+import * as S from './styles';
+
+export const SignIn = () => {
+  const [payload, setPayload] = useState({
+    email: '',
+    password: '',
+  });
+
+  const onChangeTextByField = useCallback(
+    (text: string, field: "email" | "password") => {
+      setPayload(prev => ({ ...prev, [field]: text }))
+    },
+    []
+  )
+
+  const onLogin = useCallback(() => {
+  }, [])
+
+  return (
+    <S.Wrapper>
+      <S.LoginContainer>
+        <S.LogoContainer>
+          <img src={flowbuildLogo} alt="Logo" />
+        </S.LogoContainer>
+
+        <S.Form>
+          <S.Input
+            id="email"
+            label="E-mail"
+            placeholder='Digite seu e-mail'
+            type="email"
+            value={payload.email}
+            onChange={evt => onChangeTextByField(evt.target.value, 'email')}
+          />
+
+          <S.Input
+            id="password"
+            label="Senha"
+            placeholder='Digite sua senha'
+            type="password"
+            value={payload.password}
+            onChange={evt => onChangeTextByField(evt.target.value, 'password')}
+          />
+
+          <S.PrimaryButton title="Login" fullWidth onClick={onLogin} />
+        </S.Form>
+      </S.LoginContainer>
+    </S.Wrapper>
+  );
+}
