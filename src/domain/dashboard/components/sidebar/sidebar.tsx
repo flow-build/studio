f2fd6ef91c36f0c96f5762d01d40c5fdcc42a617
Elson Oliveira <elson.oliveira@fdte.io>
2022-04-10 15:47:39 -0300

    Sidebar created

---
+++ src/domain/dashboard/components/sidebar/sidebar.tsx
@@ -0,0 +1,5 @@
+import * as S from './styles'
+
+export const Sidebar: React.FC<{}> = () => {
+  return <S.Wrapper />
+}
---
+++ src/domain/dashboard/components/sidebar/styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+import Box from '@mui/material/Box'
+
+export const Wrapper = styled(Box).attrs({
+  sx: {
+    gridArea: 'sidebar',
+    background: (theme) => theme.palette.background.paper
+  }
+})``
