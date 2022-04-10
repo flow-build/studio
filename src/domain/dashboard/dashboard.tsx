0156d1e22b1ea8dcbbe4d9954b508109c280c33c
Elson Oliveira <elson.oliveira@fdte.io>
2022-04-10 15:47:52 -0300

    Dashboard created and styled

--- src/domain/dashboard/dashboard.tsx
+++ src/domain/dashboard/dashboard.tsx
@@ -1,9 +1,19 @@
+import { Outlet } from 'react-router-dom'
+
+import { Content } from 'domain/dashboard/components/content'
+import { Header } from 'domain/dashboard/components/header'
+import { Sidebar } from 'domain/dashboard/components/sidebar'
+
 import * as S from './styles'
 
 export const Dashboard: React.FC = () => {
   return (
     <S.Wrapper>
-      <h1>Hello world</h1>
+      <Header />
+      <Sidebar />
+      <Content>
+        <Outlet />
+      </Content>
     </S.Wrapper>
   );
 }
--- src/domain/dashboard/styles.ts
+++ src/domain/dashboard/styles.ts
@@ -1,3 +1,15 @@
 import styled from 'styled-components'
 
-export const Wrapper = styled.div``
+import Box from '@mui/material/Box'
+
+export const Wrapper = styled(Box).attrs({
+  sx: {
+    display: "grid",
+    gridTemplateAreas: ` 
+                'header header'
+                'sidebar content'`,
+    gridTemplateColumns: "280px calc(100vw - 280px)",
+    gridTemplateRows: "64px calc(100vh - 64px)",
+    background: (theme) => theme.palette.background.default,
+  }
+})``
