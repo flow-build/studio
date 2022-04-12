26cb7332ed1536775c8723cd4e98e3a05d24c882
Elson Oliveira <elson.oliveira@fdte.io>
2022-04-12 00:45:02 -0300

    Workflows styled

--- src/domain/dashboard/workflows/styles.ts
+++ src/domain/dashboard/workflows/styles.ts
@@ -1,3 +1,14 @@
 import styled from 'styled-components'
 
-export const Wrapper = styled.div``
+import Grid from '@mui/material/Grid'
+
+export const Wrapper = styled(Grid).attrs({
+  container: true,
+  spacing: 2
+
+})``
+
+export const TableContainer = styled(Grid).attrs({
+  item: true,
+  xs: 12
+})``
--- src/domain/dashboard/workflows/workflows.tsx
+++ src/domain/dashboard/workflows/workflows.tsx
@@ -1,9 +1,42 @@
+import { useEffect, useState } from 'react'
+
+import _isEqual from 'lodash/isEqual'
+
+import { ModeView } from 'constants/mode-view';
+
+import { CardsView } from 'domain/dashboard/workflows/components/cards-view'
+import { Header } from 'domain/dashboard/workflows/components/header'
+import { Table } from 'domain/dashboard/workflows/components/table'
+
+import { TWorkflow } from 'models/workflow'
+
+import { listWorkflows } from 'services/resources/workflows/list'
+
 import * as S from './styles'
 
 export const Workflows: React.FC<{}> = () => {
+  const [workflows, setWorkflows] = useState<TWorkflow[]>([]);
+  const [modeView, setModeView] = useState(ModeView.LIST)
+
+  useEffect(() => {
+    const request = async () => {
+      const response = await listWorkflows()
+      setWorkflows(response.reverse())
+    }
+    request()
+  }, [])
+
   return (
     <S.Wrapper>
-      <h1>Workflows</h1>
+      <Header initialModeView={modeView} onChange={setModeView} />
+
+      {_isEqual(modeView, ModeView.CARDS) && <CardsView workflows={workflows} />}
+
+      {_isEqual(modeView, ModeView.LIST) && (
+        <S.TableContainer>
+          <Table data={workflows} />
+        </S.TableContainer>
+      )}
     </S.Wrapper>
   )
 }
