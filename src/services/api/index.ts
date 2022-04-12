9d77ad2635e9eb0a345fa4d4662b11708a7f24af
Elson Oliveira <elson.oliveira@fdte.io>
2022-04-11 23:40:57 -0300

    Interceptor to use token created

--- src/services/api/index.ts
+++ src/services/api/index.ts
@@ -1,7 +1,22 @@
 import axios from 'axios'
+import { getStorageItem } from 'shared/utils/storage';
 
 const api = axios.create({
   baseURL: process.env.REACT_APP_BASE_URL
 });
+
+api.interceptors.request.use(
+  async (config) => {
+    const token = getStorageItem('TOKEN');
+
+    if (token && config.headers) {
+      config.headers.Authorization = `Bearer ${token}`
+    }
+    return config
+  },
+  (error) => {
+    console.error(`Interceptors Request -> ${error.name}: ${error.message}`)
+  }
+)
 
 export { api }
