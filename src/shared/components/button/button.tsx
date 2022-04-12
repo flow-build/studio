8b0d88972b2050ce7060e4506046180c8df7dfdb
Elson Oliveira <elson.oliveira@fdte.io>
2022-04-11 21:21:05 -0300

    Shared button receiving dynamic variant

--- src/shared/components/button/button.tsx
+++ src/shared/components/button/button.tsx
@@ -4,12 +4,14 @@ type Props = {
   title: string;
   fullWidth?: boolean;
 
+  variant?: "contained" | "outlined" | "text";
+
   onClick?: React.MouseEventHandler<HTMLButtonElement> | undefined;
 }
 
-export const Button: React.FC<Props> = ({ title, ...props }) => {
+export const Button: React.FC<Props> = ({ title, variant = "contained", ...props }) => {
   return (
-    <S.Wrapper {...props}>
+    <S.Wrapper variant={variant} {...props}>
       {title}
     </S.Wrapper>
   );
--- src/shared/components/button/styles.ts
+++ src/shared/components/button/styles.ts
@@ -2,6 +2,4 @@ import styled from 'styled-components';
 
 import Button from '@mui/material/Button'
 
-export const Wrapper = styled(Button).attrs({
-  variant: 'contained'
-})``
+export const Wrapper = styled(Button).attrs({})``
