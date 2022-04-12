93c8530295507e42fa757ed8b872cd95cf31f1ac
Elson Oliveira <elson.oliveira@fdte.io>
2022-04-11 21:20:24 -0300

    Custom scrollbar

--- src/theme/global-style.ts
+++ src/theme/global-style.ts
@@ -25,4 +25,23 @@ export default createGlobalStyle`
     color: unset;
     text-decoration: none;
   }
+
+  ::-webkit-scrollbar {
+    width: 5px;
+  }
+
+  /* Track */
+  ::-webkit-scrollbar-track {
+    background: #f1f1f1; 
+  }
+  
+  /* Handle */
+  ::-webkit-scrollbar-thumb {
+    background: #888; 
+  }
+
+  /* Handle on hover */
+  ::-webkit-scrollbar-thumb:hover {
+    background: #555; 
+  }
 `;
