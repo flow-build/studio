d90871114c4cd0a94933cf3333d2832cc9e97363
Elson Oliveira <elson.oliveira@fdte.io>
2022-04-14 10:46:36 -0300

    Add properties to TState

--- src/models/state.ts
+++ src/models/state.ts
@@ -4,4 +4,13 @@ export type TState = {
   node_id: string;
   node_name: string;
   step_number: number;
+
+  created_at: string;
+  engine_id: string;
+  error: null; /* TODO: Rever */
+  external_input: null;
+  process_id: string;
+  result: any; /* TODO: Rever */
+  status: string; /* TODO: Rever */
+  time_elapsed: string;
 }
