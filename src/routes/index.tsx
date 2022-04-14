68e071068dc601cfe51d7fd5e8898994ec373e78
Elson Oliveira <elson.oliveira@fdte.io>
2022-04-14 10:46:26 -0300

    Router updated

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,14 @@
 import { lazy } from 'react'
-import { BrowserRouter, Route, Routes, Outlet } from 'react-router-dom'
+import { BrowserRouter, Route, Routes/* , Outlet */ } from 'react-router-dom'
 
-const Pages = {
+export const Pages = {
   Dashboard: lazy(() => import('../domain/dashboard').then(module => ({ default: module.Dashboard }))),
-  SignIn: lazy(() => import('../domain/sign/sign-in').then(module => ({ default: module.SignIn })))
+  SignIn: lazy(() => import('../domain/sign/sign-in').then(module => ({ default: module.SignIn }))),
+
+  Workflows: lazy(() => import('../domain/dashboard/workflows').then(module => ({ default: module.Workflows }))),
+  Processes: lazy(() => import('../domain/dashboard/workflows/processes').then(module => ({ default: module.Processes }))),
+  History: lazy(() => import('../domain/dashboard/workflows/processes/history').then(module => ({ default: module.History }))),
+  Diagram: lazy(() => import('../domain/dashboard/workflows/diagram').then(module => ({ default: module.Diagram })))
 }
 
 export const AppRoutes = () => {
@@ -11,7 +16,14 @@ export const AppRoutes = () => {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Pages.SignIn />} />
-        <Route path="/dashboard" element={<Pages.Dashboard />} />
+        <Route path="dashboard" element={<Pages.Dashboard />}>
+          <Route path="workflows" element={<Pages.Workflows />} />
+          <Route path="workflows/:id/processes" element={<Pages.Processes />} />
+          <Route path="workflows/:id/processes/:process_id/history" element={<Pages.History />} />
+          <Route path="workflows/:id/diagram" element={<Pages.Diagram />} />
+
+          <Route path="workflows/:id/diagram" element={<Pages.Diagram />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
