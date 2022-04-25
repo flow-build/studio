a65ae4f480162c152dc70152f7f0989531a4da38
Elson Oliveira <elson.oliveira@fdte.io>
2022-04-25 16:51:26 -0300

    List workflows receiving search text

--- src/services/resources/workflows/list/index.ts
+++ src/services/resources/workflows/list/index.ts
@@ -1,11 +1,36 @@
+import _isEmpty from 'lodash/isEmpty'
+
 import { TWorkflow } from "models/workflow";
 
 import { api } from "services/api";
 
-export async function listWorkflows(): Promise<TWorkflow[]> {
+function filterWorkflowByName(workflowName: string, name: string) {
+  return workflowName.includes(name)
+}
+
+function filterWorkflowById(workflowId: string, id: string) {
+  return workflowId.includes(id)
+}
+
+function filterWorkflow(workflow: TWorkflow, search: string) {
+  const name = workflow.name;
+  const id = workflow.workflow_id
+
+  return filterWorkflowByName(name, search) || filterWorkflowById(id, search)
+}
+
+export async function listWorkflows(params?: { search: string }): Promise<TWorkflow[]> {
   try {
-    const { data } = await api.get('/workflows');
-    return data;
+    const { data } = await api.get<TWorkflow[]>('/workflows');
+
+    if (_isEmpty(params?.search)) {
+      return data;
+    }
+
+    return data.filter(
+      workflow => filterWorkflow(workflow, params?.search.trim() as string)
+    );
+
   } catch (error) {
     throw new Error('Erro ao listar workflows')
   }
