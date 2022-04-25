e642393525affd7bdf04acd8ddce805cdc4c4ea2
Elson Oliveira <elson.oliveira@fdte.io>
2022-04-25 16:51:00 -0300

    Store moved to store folder

---
+++ src/store/index.ts
@@ -0,0 +1,36 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { setupListeners } from '@reduxjs/toolkit/query';
+import { CurriedGetDefaultMiddleware } from '@reduxjs/toolkit/dist/getDefaultMiddleware';
+
+import filterReducer from 'store/slices/filter'
+
+/* TODO: Código legado. Necessário refatorar o diagrama */
+import { bpmnService } from 'domain/dashboard/workflows/diagram/services/bpmnService';
+import { workflowService } from 'domain/dashboard/workflows/diagram/services/workflowService';
+import { bpmnSliceReducer, notificationsSliceReducer } from 'domain/dashboard/workflows/diagram/features';
+
+/* TODO: Código legado. Necessário refatorar o diagrama */
+const legacyReducer = {
+  [workflowService.reducerPath]: workflowService.reducer,
+  [bpmnService.reducerPath]: bpmnService.reducer,
+  bpmn: bpmnSliceReducer,
+  notifications: notificationsSliceReducer
+}
+
+/* TODO: Código legado. Necessário refatorar o diagrama */
+const legacyMiddleware = (getDefaultMiddleware: CurriedGetDefaultMiddleware<any>) => getDefaultMiddleware().concat([
+  workflowService.middleware,
+  bpmnService.middleware
+])
+
+export const store = configureStore({
+  reducer: {
+    filter: filterReducer,
+    ...legacyReducer
+  },
+  middleware: legacyMiddleware
+});
+
+export type RootState = ReturnType<typeof store.getState>
+
+setupListeners(store.dispatch);
