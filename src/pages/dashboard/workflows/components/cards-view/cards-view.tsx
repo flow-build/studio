dc6488a522bd12cbd03af84eb4cea2a3a11447d3
Elson Oliveira <elson.oliveira@fdte.io>
2022-04-25 17:21:55 -0300

    Cards view using cards info shared component

--- src/pages/dashboard/workflows/components/cards-view/cards-view.tsx
+++ src/pages/dashboard/workflows/components/cards-view/cards-view.tsx
@@ -1,10 +1,6 @@
-import { AddOutlined, ExtensionOutlined, VisibilityOutlined } from '@mui/icons-material'
-
 import { TWorkflow } from 'models/workflow'
 
-import { IconButton } from 'shared/components/icon-button'
 import { getLongFormatByDate } from 'shared/utils/date'
-
 
 import * as S from './styles'
 
@@ -16,28 +12,14 @@ export const CardsView: React.FC<Props> = ({ workflows }) => {
   return (
     <>
       {workflows.map(workflow => (
-        <S.Wrapper key={workflow.workflow_id}>
-          <S.Card>
-            <S.Content>
-              <S.Subtitle>Versão {workflow.version}</S.Subtitle>
-
-              <S.Title>{workflow.name}</S.Title>
-              <S.Subtitle>{workflow.workflow_id}</S.Subtitle>
-
-              <S.Description>{workflow.description}</S.Description>
-            </S.Content>
-
-            <S.Actions>
-              <S.Caption>{getLongFormatByDate(workflow.created_at)}</S.Caption>
-
-              <div>
-                <IconButton icon={VisibilityOutlined} tooltip="Ver processos" />
-                <IconButton icon={AddOutlined} tooltip="Novo processos" />
-                <IconButton icon={ExtensionOutlined} tooltip="Ver diagrama" />
-              </div>
-            </S.Actions>
-          </S.Card>
-        </S.Wrapper>
+        <S.Cards
+          key={workflow.workflow_id}
+          title={workflow.name}
+          subtitle={workflow.workflow_id}
+          description={workflow.description}
+          headerTitle={`Versão ${workflow.version}`}
+          footerTitle={getLongFormatByDate(workflow.created_at)}
+        />
       ))}
     </>
   )
