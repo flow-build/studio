603984f935892ff905e81214327e90f0ba668aac
Elson Oliveira <elson.oliveira@fdte.io>
2022-04-26 10:36:06 -0300

    Hook to handle table on workflow page created

---
+++ src/pages/dashboard/workflows/hooks/useTable.ts
@@ -0,0 +1,60 @@
+import { useMemo } from "react"
+import { AddOutlined, ExtensionOutlined, VisibilityOutlined } from '@mui/icons-material'
+
+import { useWorkflowPage } from "pages/dashboard/workflows/hooks/useWorkflowPage"
+
+import { TWorkflow } from "models/workflow"
+
+import { getLongFormatByDate } from "shared/utils/date"
+
+export function useTable(workflows: TWorkflow[]) {
+  const workflowPage = useWorkflowPage()
+
+  const columnData = useMemo(() => {
+    return [
+      { id: 'name', name: 'Name' },
+      { id: 'id', name: 'ID' },
+      { id: 'description', name: 'Description' },
+      { id: 'version', name: 'Version' },
+      { id: 'createdAt', name: 'Created at' },
+      { id: 'actions', name: 'Actions' },
+    ]
+  }, [])
+
+  const rows = useMemo(() => {
+    return workflows.map(workflow => {
+      const items = [
+        workflow.name,
+        workflow.workflow_id,
+        workflow.description,
+        workflow.version,
+        getLongFormatByDate(workflow.created_at)
+      ];
+
+      const actions = [
+        {
+          icon: VisibilityOutlined,
+          tooltip: 'Ver processos',
+          onClick: () => workflowPage.navigateToProcess(workflow.workflow_id)
+        },
+        {
+          icon: AddOutlined,
+          tooltip: 'Novo processo',
+          onClick: () => workflowPage.createProcess(workflow.name, workflow.workflow_id)
+        },
+        {
+          icon: ExtensionOutlined,
+          tooltip: 'Ver diagrama',
+          onClick: () => workflowPage.navigateToDiagram(workflow.workflow_id)
+        }
+      ]
+
+      return { items, actions }
+    })
+  }, [workflowPage, workflows])
+
+  return {
+    rows,
+    columnData,
+  }
+}
