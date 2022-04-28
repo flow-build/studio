66344a8fe947d8c23e9a95ecf5f6ca0fe371a1af
Elson Oliveira <elson.oliveira@fdte.io>
2022-04-27 22:37:09 -0300

    Processes page using shared table

--- src/pages/processes/processes.tsx
+++ src/pages/processes/processes.tsx
@@ -1,5 +1,6 @@
 import { useCallback, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
+import { Typography } from '@mui/material'
 
 import _isEqual from 'lodash/isEqual'
 import _isNull from 'lodash/isNull'
@@ -7,8 +8,7 @@ import _isNull from 'lodash/isNull'
 import { ModeView } from 'constants/mode-view'
 
 import { CardsView } from 'pages/processes/components/cards-view'
-import { Header } from 'pages/processes/components/header'
-import { Table } from 'pages/processes/components/table'
+import { useTable } from 'pages/processes/hooks/useTable'
 
 import { TProcess } from 'models/process'
 import { TWorkflow } from 'models/workflow'
@@ -16,9 +16,9 @@ import { TWorkflow } from 'models/workflow'
 import { listByProcessId } from 'services/resources/processes/list-by-process-id'
 import { listWorkflowById } from 'services/resources/workflows/list-by-id'
 
-import * as S from './styles'
-import { Typography } from '@mui/material'
 import { ContentHeader } from 'shared/components/content-header'
+
+import * as S from './styles'
 
 type TPayload = {
   processes: TProcess[] | null;
@@ -30,6 +30,8 @@ export const Processes: React.FC<{}> = () => {
 
   const [modeView, setModeView] = useState(ModeView.LIST)
   const [payload, setPayload] = useState<TPayload>({ processes: null, workflow: null })
+
+  const table = useTable(payload.processes ?? [])
 
   const getProcessesInformation = useCallback(async (workflowId: string) => {
     const response = await listByProcessId(workflowId)
@@ -71,7 +73,7 @@ export const Processes: React.FC<{}> = () => {
 
       {_isEqual(modeView, ModeView.LIST) && (
         <S.TableContainer>
-          <Table data={payload.processes} />
+          <S.Table columnData={table.columnData} rows={table.rows} />
         </S.TableContainer>
       )}
     </S.Wrapper>
