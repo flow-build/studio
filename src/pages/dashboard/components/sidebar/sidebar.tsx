ad303c3bc39821969a193f91dda50040a863dc90
Elson Oliveira <elson.oliveira@fdte.io>
2022-04-28 16:40:15 -0300

    Sidebar updated to use drawer

--- src/pages/dashboard/components/sidebar/sidebar.tsx
+++ src/pages/dashboard/components/sidebar/sidebar.tsx
@@ -5,21 +5,106 @@ import { useSidebar } from 'pages/dashboard/components/sidebar/hooks/useSidebar'
 
 import * as S from './styles'
 
-export const Sidebar: React.FC<{}> = () => {
+
+import { styled, useTheme, Theme, CSSObject } from '@mui/material/styles';
+import MuiDrawer from '@mui/material/Drawer';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+
+const drawerWidth = 240;
+
+const openedMixin = (theme: Theme): CSSObject => ({
+  width: drawerWidth,
+  transition: theme.transitions.create('width', {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.enteringScreen,
+  }),
+  overflowX: 'hidden',
+});
+
+const closedMixin = (theme: Theme): CSSObject => ({
+  transition: theme.transitions.create('width', {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  overflowX: 'hidden',
+  width: `calc(${theme.spacing(7)} + 1px)`,
+  [theme.breakpoints.up('sm')]: {
+    width: `calc(${theme.spacing(8)} + 1px)`,
+  },
+});
+const DrawerHeader = styled('div')(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'flex-end',
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+}));
+const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
+  ({ theme, open }) => ({
+    width: drawerWidth,
+    flexShrink: 0,
+    whiteSpace: 'nowrap',
+    boxSizing: 'border-box',
+    ...(open && {
+      ...openedMixin(theme),
+      '& .MuiDrawer-paper': openedMixin(theme),
+    }),
+    ...(!open && {
+      ...closedMixin(theme),
+      '& .MuiDrawer-paper': closedMixin(theme),
+    }),
+  }),
+);
+
+type Props = {
+  isOpen: boolean;
+  onClose?: React.MouseEventHandler<HTMLButtonElement>;
+}
+
+export const Sidebar: React.FC<Props> = ({ isOpen, onClose }) => {
+  const theme = useTheme();
   const sidebar = useSidebar()
 
   return (
-    <S.Wrapper>
-      <S.NavList>
+    <Drawer variant="permanent" open={isOpen}>
+      <DrawerHeader>
+        <IconButton onClick={onClose}>
+          {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
+        </IconButton>
+      </DrawerHeader>
+      <Divider />
+      <List>
         {sidebar.menuItems.map((menuItem, index) => (
-          <Link to={menuItem.pathname} key={index.toString()}>
-            <S.MenuItem>
-              <S.MenuIcon>{menuItem.icon}</S.MenuIcon>
-              <S.MenuText>{menuItem.name}</S.MenuText>
-            </S.MenuItem>
+          <Link key={index.toString()} to={menuItem.pathname}>
+            <ListItemButton
+              sx={{
+                minHeight: 48,
+                justifyContent: isOpen ? 'initial' : 'center',
+                px: 2.5,
+              }}
+            >
+              <ListItemIcon
+                sx={{
+                  minWidth: 0,
+                  mr: isOpen ? 3 : 'auto',
+                  justifyContent: 'center',
+                }}
+              >
+                {menuItem.icon}
+              </ListItemIcon>
+              <ListItemText primary={menuItem.name} sx={{ opacity: isOpen ? 1 : 0 }} />
+            </ListItemButton>
           </Link>
         ))}
-      </S.NavList>
-    </S.Wrapper>
+      </List>
+    </Drawer>
   )
 }
