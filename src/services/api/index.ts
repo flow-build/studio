02f8becc4271269742f63aa6fe47338d16804b49
Elson Oliveira <elson.oliveira@fdte.io>
2022-04-28 16:41:18 -0300

    Verify expired token

--- src/services/api/index.ts
+++ src/services/api/index.ts
@@ -1,9 +1,24 @@
 import axios from 'axios'
-import { getStorageItem } from 'shared/utils/storage';
+
+import jwt_decode from "jwt-decode";
+import { getAnonymousToken } from 'services/resources/token';
+
+import { getStorageItem, setStorageItem } from 'shared/utils/storage';
 
 const api = axios.create({
   baseURL: process.env.REACT_APP_BASE_URL
 });
+
+type TToken = {
+  exp: number;
+}
+
+function isTokenExpired(token: string) {
+  const { exp } = jwt_decode<TToken>(token)
+  const expireDate = new Date(exp * 1000).getTime()
+
+  return Date.now() > expireDate;
+}
 
 api.interceptors.request.use(
   async (config) => {
@@ -12,10 +27,30 @@ api.interceptors.request.use(
     if (token && config.headers) {
       config.headers.Authorization = `Bearer ${token}`
     }
+
     return config
   },
   (error) => {
     console.error(`Interceptors Request -> ${error.name}: ${error.message}`)
+  }
+)
+
+api.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async (error) => {
+    let token = getStorageItem('TOKEN');
+
+    if (token && isTokenExpired(token)) {
+      token = await getAnonymousToken()
+      setStorageItem('TOKEN', token);
+
+      error.config.headers['Authorization'] = 'Bearer ' + token;
+      return api.request(error.config)
+    }
+
+    return Promise.reject(error)
   }
 )
 
