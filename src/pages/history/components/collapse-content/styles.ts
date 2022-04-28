f1f0436c739573af75c8fe01604d47df78750022
Elson Oliveira <elson.oliveira@fdte.io>
2022-04-27 23:40:44 -0300

    Collapse content created

---
+++ src/pages/history/components/collapse-content/styles.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+import MuiTable from '@mui/material/Table'
+import MuiTableCell from '@mui/material/TableCell'
+import MuiTableBody from '@mui/material/TableBody'
+import MuiTableHead from '@mui/material/TableHead'
+import MuiTableRow from '@mui/material/TableRow'
+
+export const Table = styled(MuiTable)``
+
+export const TableCell = styled(MuiTableCell)``
+
+export const TableBody = styled(MuiTableBody)``
+
+export const TableHead = styled(MuiTableHead)``
+
+export const TableRow = styled(MuiTableRow)``
