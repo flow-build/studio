8739928c8fc5326af9635cc417eabdf7001f7609
Elson Oliveira <elson.oliveira@fdte.io>
2022-04-28 16:41:10 -0300

    Fix content style

--- src/shared/components/content/styles.ts
+++ src/shared/components/content/styles.ts
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 import Box from '@mui/material/Box'
 
 export const Wrapper = styled(Box)`
-  grid-area: content;
   overflow: auto;
+  flex-grow: 1;
+  height: 100vh;
 `
