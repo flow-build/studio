3d791a096234e7ffe7f17c7b170e71cfbaad56c3
Elson Oliveira <elson.oliveira@fdte.io>
2022-05-17 01:24:00 -0300

    menu item added to navigate to compare json

--- src/pages/dashboard/components/sidebar/hooks/useSidebar.tsx
+++ src/pages/dashboard/components/sidebar/hooks/useSidebar.tsx
@@ -1,5 +1,6 @@
 import { useMemo } from "react"
 
+import CompareArrowsIcon from '@mui/icons-material/CompareArrows';
 import SchemaIcon from '@mui/icons-material/Schema';
 import SearchIcon from '@mui/icons-material/Search';
 
@@ -10,6 +11,11 @@ export function useSidebar() {
         name: 'Workflows',
         pathname: 'workflows',
         icon: <SchemaIcon />,
+      },
+      {
+        name: 'Comparar States',
+        pathname: 'compare-json',
+        icon: <CompareArrowsIcon />,
       },
       {
         name: 'Buscar',
