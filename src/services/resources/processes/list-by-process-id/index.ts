53d1fb8e971a3f6bff5bccf1050b4a0e53416765
Elson Oliveira <elson.oliveira@fdte.io>
2022-06-02 14:34:08 -0300

    Navigating between subprocesses

--- src/services/resources/processes/list-by-process-id/index.ts
+++ src/services/resources/processes/list-by-process-id/index.ts
@@ -1,9 +1,9 @@
 import { TProcess } from "models/process";
 import { api } from "services/api"
 
-export async function listByProcessId(processId: string): Promise<TProcess[]> {
+export async function listByProcessId(workflowId: string): Promise<TProcess[]> {
   try {
-    const { data } = await api.get(`/workflows/${processId}/processes`)
+    const { data } = await api.get(`/workflows/${workflowId}/processes`)
     return data;
   } catch (error) {
     throw new Error("Erro ao listar processo pelo processId")
