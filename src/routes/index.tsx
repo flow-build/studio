48e74552a62948e262c84d24139034e7b1020e30
Elson Oliveira <elson.oliveira@fdte.io>
2022-07-13 17:29:58 -0300

    refactor: removed legacy diagram from route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,27 +1,14 @@
-// import { lazy } from 'react'
-import { BrowserRouter, Route, Routes/* , Outlet */ } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import { Dashboard } from 'pages/dashboard'
-import { SignIn } from 'pages/sign/sign-in'
+import { Dashboard } from "pages/dashboard";
+import { SignIn } from "pages/sign/sign-in";
 
-import { Search } from 'pages/search'
-import { Workflows } from 'pages/workflows'
-import { CompareJson } from 'pages/compare-json'
-import { Processes } from 'pages/processes'
-import { History } from 'pages/history'
-import { Diagram } from 'pages/diagram'
-
-/* export const Pages = {
-  Dashboard: lazy(() => import('../pages/dashboard').then(module => ({ default: module.Dashboard }))),
-  SignIn: lazy(() => import('../pages/sign/sign-in').then(module => ({ default: module.SignIn }))),
-
-  Search: lazy(() => import('../pages/search').then(module => ({ default: module.Search }))),
-
-  Workflows: lazy(() => import('../pages/workflows').then(module => ({ default: module.Workflows }))),
-  Processes: lazy(() => import('../pages/processes').then(module => ({ default: module.Processes }))),
-  History: lazy(() => import('../pages/history').then(module => ({ default: module.History }))),
-  Diagram: lazy(() => import('../pages/diagram').then(module => ({ default: module.Diagram })))
-} */
+import { Search } from "pages/search";
+import { Workflows } from "pages/workflows";
+import { CompareJson } from "pages/compare-json";
+import { Processes } from "pages/processes";
+import { History } from "pages/history";
+import { DiagramRefactored } from "pages/diagram";
 
 export const AppRoutes = () => {
   return (
@@ -31,8 +18,14 @@ export const AppRoutes = () => {
         <Route path="dashboard" element={<Dashboard />}>
           <Route path="workflows" element={<Workflows />} />
           <Route path="workflows/:id/processes" element={<Processes />} />
-          <Route path="workflows/:id/processes/:process_id/history" element={<History />} />
-          <Route path="workflows/:id/diagram" element={<Diagram />} />
+          <Route
+            path="workflows/:id/processes/:process_id/history"
+            element={<History />}
+          />
+          <Route
+            path="workflows/:workflowId/diagram"
+            element={<DiagramRefactored />}
+          />
 
           <Route path="compare-json" element={<CompareJson />} />
 
@@ -41,4 +34,4 @@ export const AppRoutes = () => {
       </Routes>
     </BrowserRouter>
   );
-}
+};
