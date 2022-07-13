9985e18342e84f4968863848e301460d61a7206f
Elson Oliveira <elson.oliveira@fdte.io>
2022-07-13 17:26:50 -0300

    refactor: hook to handle with diagram functionalities created

--- src/pages/diagram/hooks/useDiagram.ts
+++ src/pages/diagram/hooks/useDiagram.ts
@@ -1,62 +1,140 @@
-import { contrastingColor, statusColors } from "../utils/statusColors";
+import { useCallback, useEffect, useRef, useState } from "react";
+import { useSnackbar, OptionsObject } from "notistack";
+import Modeler from "bpmn-js/lib/Modeler";
+
+import { useTheme } from "@mui/material/styles";
+
+import _isEmpty from "lodash/isEmpty";
+
+import { listByWorkflowId } from "services/resources/diagrams/list-by-workflow-id";
+
+interface IColor {
+  backgroundColor?: string;
+  borderColor?: string;
+  fill?: string;
+  stroke?: string;
+}
+
+interface IElement {
+  teste: any;
+  color: IColor;
+}
+
+let bpmnViewer: any = null;
 
 export function useDiagram() {
-  const handleZoomIn = (modeler: any) => {
-    if (!modeler) return;
+  const theme = useTheme();
 
-    modeler.get("zoomScroll").stepZoom(1);
-  };
+  const diagramBPMN = useRef();
 
-  const handleZoomOut = (modeler: any) => {
-    if (!modeler) return;
+  const [initialElements, setInitialElements] = useState<IElement[]>([]);
+  const [diagramXML, setDiagramXML] = useState<any>();
 
-    modeler.get("zoomScroll").stepZoom(-1);
-  };
+  const { enqueueSnackbar } = useSnackbar();
 
-  const handleOnSaveXML = async (modeler: any) => {
+  async function downloadXML(modeler: any) {
     try {
-      const { xml } = await modeler.saveXML({ format: true });
-
+      const response = await modeler.saveXML({ format: true });
+      const file = new Blob([response.xml], { type: "text/xml" });
       const canvas = modeler.get("canvas");
-      const blob = new Blob([xml], { type: "text/xml" });
-      const link = document.createElement("a");
 
-      link.setAttribute("href", URL.createObjectURL(blob));
-      link.setAttribute(
-        "download",
-        canvas.getRootElement().businessObject["id" || "name"]
-      );
+      const element = document.createElement("a");
+      element.href = URL.createObjectURL(file);
+      element.download = canvas.getRootElement().businessObject["id" || "name"];
 
-      link.style.display = "none";
+      document.body.appendChild(element);
 
-      link.click();
-    } catch (e: any) {
-      console.error(
-        `Hooks/useDiagram/handleOnSaveXML => ${e.error}: ${e.message}`
-      );
+      element.click();
+    } catch (error) {
+      const message = "Erro ao baixar diagrama em XML";
+      const options: OptionsObject = {
+        autoHideDuration: 2000,
+        variant: "error",
+      };
+
+      enqueueSnackbar(message, options);
     }
   }
 
-  const handleDrawOnDiagram = (modeler: any, data: any) => {
-    if (!modeler && !data) return
+  const loadDiagram = useCallback(async (id: string) => {
+    const response = await listByWorkflowId(id);
+    setDiagramXML(response);
+  }, []);
 
-    const modeling = modeler.get('modeling')
-    const elementRegistry = modeler.get('elementRegistry')
+  const createModeler = useCallback(() => {
+    return new Modeler({
+      container: diagramBPMN.current,
+      bpmnRenderer: {
+        defaultFillColor: theme?.palette?.background?.default,
+        defaultStrokeColor: theme?.palette?.common?.white,
+      },
+    }) as any;
+  }, [theme?.palette?.background?.default, theme?.palette?.common?.white]);
 
-    data.forEach((history: any) => {
-      const element = elementRegistry.get(`Node_${history.node_id}`)
+  const onImportDone = useCallback((event: any) => {
+    const { error /* , warnings */ } = event;
 
-      modeling.setColor(element, {
-        fill: statusColors[history.status],
-        stroke: contrastingColor(statusColors[`${history.status}`])
-      })
-    })
-  }
+    if (error) {
+      // return this.handleError(error);
+    }
+
+    bpmnViewer.get("canvas").zoom("fit-viewport", "auto");
+    bpmnViewer.get("canvas").zoom(0.6);
+
+    const arr: Array<IElement> = [];
+    const elements = bpmnViewer.get("elementRegistry").getAll() as Array<any>;
+    elements.forEach((element) => {
+      const color: IColor = {
+        backgroundColor: undefined,
+        borderColor: undefined,
+        fill: undefined,
+        stroke: undefined,
+      };
+
+      if (element.di["background-color"]) {
+        color.backgroundColor = element.di["background-color"];
+      }
+
+      if (element.di["border-color"]) {
+        color.borderColor = element.di["border-color"];
+      }
+
+      if (element.di["fill"]) {
+        color.fill = element.di["fill"];
+      }
+
+      if (element.di["stroke"]) {
+        color.stroke = element.di["stroke"];
+      }
+
+      arr.push({
+        teste: element,
+        color,
+      });
+    });
+
+    setInitialElements(arr);
+  }, []);
+
+  useEffect(() => {
+    if (diagramBPMN?.current) {
+      bpmnViewer = createModeler();
+
+      bpmnViewer.on("import.done", onImportDone);
+
+      if (!_isEmpty(diagramXML)) {
+        bpmnViewer.importXML(diagramXML);
+      }
+    }
+
+    return () => bpmnViewer?.destroy();
+  }, [createModeler, diagramBPMN, diagramXML, onImportDone]);
 
   return {
-    handleZoomIn,
-    handleZoomOut,
-    handleOnSaveXML,
-    handleDrawOnDiagram
-  }
+    downloadXML,
+    loadDiagram,
+    bpmn: diagramBPMN,
+    modeler: bpmnViewer,
+    initialElements,
+  };
 }
