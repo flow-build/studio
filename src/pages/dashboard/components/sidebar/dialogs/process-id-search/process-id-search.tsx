52563cd0336ae4c10236ba70cd521bd9b0651b1b
Elson Oliveira <elson.oliveira@fdte.io>
2022-07-25 19:51:41 -0300

    fix: remove depreciated code

--- src/pages/dashboard/components/sidebar/dialogs/process-id-search/process-id-search.tsx
+++ src/pages/dashboard/components/sidebar/dialogs/process-id-search/process-id-search.tsx
@@ -1,18 +1,16 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { useDispatch } from "react-redux";
+// import { useDispatch } from "react-redux";
 
 import { listStatesByProcessId } from "services/resources/processes/list-states";
-import {
-  setSearchProcessIdDialog,
-  setSearchProcessIdDialogData,
-} from "pages/diagram/features/bpmnSlice";
+// import {
+//   setSearchProcessIdDialog,
+//   setSearchProcessIdDialogData,
+// } from "pages/diagram/features/bpmnSlice";
 import { IPayload } from "pages/dashboard/components/sidebar/dialogs/process-id-search/types/IPayload";
-
 
 import { DialogActions } from "@mui/material";
 import * as S from "./styles";
-
 
 type Props = {
   isOpen: boolean;
@@ -21,7 +19,7 @@ type Props = {
 
 export const ProcessIdSearch: React.FC<Props> = ({ isOpen, onClose }) => {
   const navigate = useNavigate();
-  const dispatch = useDispatch();
+  // const dispatch = useDispatch();
 
   const [payload, setPayload] = useState<IPayload>({
     processId: "",
@@ -35,14 +33,13 @@ export const ProcessIdSearch: React.FC<Props> = ({ isOpen, onClose }) => {
     const response = await listStatesByProcessId(payload.processId);
 
     /* TODO: Remover legacy code */
-    dispatch(setSearchProcessIdDialogData(response));
-    dispatch(setSearchProcessIdDialog(true));
+    // dispatch(setSearchProcessIdDialogData(response));
+    // dispatch(setSearchProcessIdDialog(true));
 
     navigate(`/dashboard/workflows/${response.workflow_id}/diagram`);
 
     onClose();
   }
-
 
   return (
     <S.Dialog open={isOpen}>
