bd249cacdf811a95dd9bd3b6c70f53417a8a677e
Gabriela Sales <gabriela.sales@fdte.io>
2022-07-26 14:30:52 -0300

    return component in collapse content

--- src/pages/history/components/collapse-content/collapse-content.tsx
+++ src/pages/history/components/collapse-content/collapse-content.tsx
@@ -1,133 +1,37 @@
-import AceEditor from "react-ace/";
-import "ace-builds/src-noconflict/mode-json";
-import "ace-builds/src-noconflict/theme-clouds_midnight";
-import "ace-builds/src-noconflict/theme-solarized_dark";
-import "ace-builds/src-noconflict/theme-dracula";
-
 import _isEmpty from "lodash/isEmpty";
 
 import { TState } from "models/state";
+import { TableContent } from "../table-content";
 
 import * as S from "./styles";
-import ContentCopyOutlinedIcon from "@mui/icons-material/ContentCopyOutlined";
-import { IconButton } from "shared/components/icon-button";
 
 type Props = {
   state: TState;
 };
 
 export const CollapseContent: React.FC<Props> = ({ state }) => {
-  function onClickCopyBag() {
-    navigator.clipboard.writeText(JSON.stringify(state.bag, null, "\t"));
-  }
-
-  function onClickCopyResult() {
-    navigator.clipboard.writeText(JSON.stringify(state.result, null, "\t"));
-  }
-
-  function onClickCopyActor() {
-    navigator.clipboard.writeText(JSON.stringify(state.actor_data, null, "\t"));
-  }
-
   if (_isEmpty(state.result)) {
     return (
       <S.BoxTable>
         <S.TitleTable>Result</S.TitleTable>
-        <S.TextTable variant="caption" component="p">
-          No result data...
-        </S.TextTable>
+        <S.TextTable>No result data...</S.TextTable>
       </S.BoxTable>
     );
   }
 
   return (
     <>
-      <S.BoxTable>
-        <S.TitleTable>Bag</S.TitleTable>
-        <IconButton
-          icon={ContentCopyOutlinedIcon}
-          onClick={onClickCopyBag}
-          tooltip="copiar"
-        ></IconButton>
-        <S.Table aria-label="bag">
-          <S.TableBody>
-            <S.TableRow>
-              <AceEditor
-                name="Bag"
-                value={JSON.stringify(state.bag, null, "\t")}
-                mode="json"
-                theme="solarized_dark"
-                setOptions={{
-                  overwrite: false,
-                  showPrintMargin: false,
-                  copyWithEmptySelection: true,
-                }}
-                height="400px"
-                width="400px"
-                readOnly
-              />
-            </S.TableRow>
-          </S.TableBody>
-        </S.Table>
-      </S.BoxTable>
-
-      <S.BoxTable>
-        <S.TitleTable>Result</S.TitleTable>
-        <IconButton
-          icon={ContentCopyOutlinedIcon}
-          onClick={onClickCopyResult}
-          tooltip="copiar"
-        ></IconButton>
-        <S.Table aria-label="result">
-          <S.TableBody>
-            <S.TableRow>
-              <AceEditor
-                name="Result"
-                value={JSON.stringify(state.result, null, "\t")}
-                mode="json"
-                theme="clouds_midnight"
-                setOptions={{
-                  overwrite: false,
-                  showPrintMargin: false,
-                  copyWithEmptySelection: true,
-                }}
-                height="400px"
-                width="400px"
-                readOnly
-              />
-            </S.TableRow>
-          </S.TableBody>
-        </S.Table>
-      </S.BoxTable>
-
-      <S.BoxTable>
-        <S.TitleTable>Actor data</S.TitleTable>
-        <IconButton
-          icon={ContentCopyOutlinedIcon}
-          onClick={onClickCopyActor}
-          tooltip="copiar"
-        ></IconButton>
-        <S.Table aria-label="actor data">
-          <S.TableBody>
-            <S.TableRow>
-              <AceEditor
-                name="Actor Data"
-                value={JSON.stringify(state?.actor_data, null, "\t")}
-                mode="json"
-                theme="dracula"
-                setOptions={{
-                  overwrite: false,
-                  showPrintMargin: false,
-                  copyWithEmptySelection: true,
-                }}
-                height="400px"
-                width="400px"
-                readOnly
-              />
-            </S.TableRow>
-          </S.TableBody>
-        </S.Table>
-      </S.BoxTable>
+      <TableContent title="Bag" copyValue={state.bag} editorValue={state.bag} />
+      <TableContent
+        title="Result"
+        copyValue={state.result}
+        editorValue={state.result}
+      />
+      <TableContent
+        title="Actor data"
+        copyValue={state.actor_data}
+        editorValue={state.actor_data}
+      />
     </>
   );
 };
--- src/pages/history/components/collapse-content/styles.ts
+++ src/pages/history/components/collapse-content/styles.ts
@@ -7,14 +7,19 @@ import MuiTableCell from "@mui/material/TableCell";
 import MuiTableBody from "@mui/material/TableBody";
 import MuiTableHead from "@mui/material/TableHead";
 import MuiTableRow from "@mui/material/TableRow";
-
+import IconButton from "@mui/material/IconButton";
+import ContentCopyOutlinedIcon from "@mui/icons-material/ContentCopyOutlined";
+import AceEditor from "react-ace/";
+import "ace-builds/src-noconflict/mode-json";
+import "ace-builds/src-noconflict/theme-dracula";
+import "ace-builds/src-noconflict/ace";
 
 export const BoxTable = styled(Box).attrs({
   margin: 1,
   mr: 6,
   mb: 6,
   display: "inline-block",
-  justifyContent: "space-around",
+  justifyContent: "space-between",
   width: 400,
   height: 400,
 })``;
@@ -31,6 +36,13 @@ export const TextTable = styled(Typography).attrs({
   component: "p",
 })``;
 
+export const CopyIcon = styled(IconButton).attrs({
+  icon: { ContentCopyOutlinedIcon },
+  sx: {
+    tooltip: "copiar",
+  },
+})``;
+
 export const Table = styled(MuiTable)`
   size: "small";
 `;
@@ -41,3 +53,16 @@ export const TableBody = styled(MuiTableBody)``;
 export const TableHead = styled(MuiTableHead)``;
 
 export const TableRow = styled(MuiTableRow)``;
+
+export const Editor = styled(AceEditor).attrs({
+  mode: "json",
+  theme: "dracula",
+  setOptions: {
+    overwrite: false,
+    showPrintMargin: false,
+    copyWithEmptySelection: true,
+    readOnly: true,
+  },
+  height: "400px",
+  width: "400px",
+})``;
