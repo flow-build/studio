a688329133cdd5d1fcccf98d3e18b0f1e683473d
Elson Oliveira <elson.oliveira@fdte.io>
2022-07-28 18:15:26 -0300

    fix: dialog to search process id

--- src/pages/dashboard/components/sidebar/dialogs/process-id-search/process-id-search.tsx
+++ src/pages/dashboard/components/sidebar/dialogs/process-id-search/process-id-search.tsx
@@ -1,15 +1,15 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-// import { useDispatch } from "react-redux";
-
-import { listStatesByProcessId } from "services/resources/processes/list-states";
-// import {
-//   setSearchProcessIdDialog,
-//   setSearchProcessIdDialogData,
-// } from "pages/diagram/features/bpmnSlice";
-import { IPayload } from "pages/dashboard/components/sidebar/dialogs/process-id-search/types/IPayload";
+import { useDispatch } from "react-redux";
 
 import { DialogActions } from "@mui/material";
+
+import { IPayload } from "pages/dashboard/components/sidebar/dialogs/process-id-search/types/IPayload";
+
+import { listStatesByProcessId } from "services/resources/processes/list-states";
+
+import { setProcessSelected } from "store/slices/diagram";
+
 import * as S from "./styles";
 
 type Props = {
@@ -19,7 +19,7 @@ type Props = {
 
 export const ProcessIdSearch: React.FC<Props> = ({ isOpen, onClose }) => {
   const navigate = useNavigate();
-  // const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
   const [payload, setPayload] = useState<IPayload>({
     processId: "",
@@ -31,18 +31,14 @@ export const ProcessIdSearch: React.FC<Props> = ({ isOpen, onClose }) => {
 
   async function handleClickProcessId() {
     const response = await listStatesByProcessId(payload.processId);
-
-    /* TODO: Remover legacy code */
-    // dispatch(setSearchProcessIdDialogData(response));
-    // dispatch(setSearchProcessIdDialog(true));
+    dispatch(setProcessSelected(response));
 
     navigate(`/dashboard/workflows/${response.workflow_id}/diagram`);
-
     onClose();
   }
 
   return (
-    <S.Dialog open={isOpen}>
+    <S.Dialog open={isOpen} onClose={onClose}>
       <S.Title>Consultar </S.Title>
       <S.Content>
         <S.Input
