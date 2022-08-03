e85113e531addf61226d56fb5cdeefd1cd87a0a5
Elson Oliveira <elson.oliveira@fdte.io>
2022-08-03 17:21:00 -0300

    feat: pagination on table

--- src/pages/workflows/styles.ts
+++ src/pages/workflows/styles.ts
@@ -7,11 +7,17 @@ import { Table as SharedTable } from "shared/components/table";
 export const Wrapper = styled(Grid).attrs({
   container: true,
   spacing: 2,
-})``;
+  sx: { marginTop: 0 },
+})`
+  height: calc(100% - 64px);
+`;
 
 export const TableContainer = styled(Grid).attrs({
   item: true,
   xs: 12,
-})``;
+})`
+  height: calc(100% - 118px);
+  overflow-y: auto;
+`;
 
 export const Table = styled(SharedTable)``;
