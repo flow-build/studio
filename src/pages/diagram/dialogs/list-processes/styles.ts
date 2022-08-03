d269a6e9133c36fb80e0dcd499f2a32427d3845d
Elson Oliveira <elson.oliveira@fdte.io>
2022-08-03 17:23:45 -0300

    fix: unused buttons removed and close icons added

--- src/pages/diagram/dialogs/list-processes/styles.ts
+++ src/pages/diagram/dialogs/list-processes/styles.ts
@@ -6,6 +6,9 @@ import DialogContentText from "@mui/material/DialogContentText";
 import DialogTitle from "@mui/material/DialogTitle";
 
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import CloseIcon from "@mui/icons-material/Close";
+
+import { IconButton } from "shared/components/icon-button";
 
 export const Wrapper = styled(Dialog).attrs({
   "aria-labelledby": "List process dialog",
@@ -15,7 +18,16 @@ export const Wrapper = styled(Dialog).attrs({
   maxWidth: "md",
 })``;
 
-export const Title = styled(DialogTitle)``;
+export const Title = styled(DialogTitle)`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const CloseButton = styled(IconButton).attrs({
+  icon: CloseIcon,
+  ariaLabel: "close",
+})``;
 
 export const Text = styled(DialogContentText)``;
 
