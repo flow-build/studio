e964c0b95b260e9691de931649048b9b32260b6b
Gabriela Sales <gabriela.sales@fdte.io>
2022-08-08 12:25:49 -0300

    added input and date picker

--- src/pages/diagram/dialogs/list-processes/list-processes.tsx
+++ src/pages/diagram/dialogs/list-processes/list-processes.tsx
@@ -10,6 +10,7 @@ import { TProcess } from "models/process";
 import { listByWorkflowId } from "services/resources/processes/list-by-process-id";
 
 import { getLongFormatByDate } from "shared/utils/date";
+import AdapterDateFns from "@mui/x-date-pickers/AdapterDateFns";
 
 import * as S from "./styles";
 
@@ -45,6 +46,13 @@ export const ListProcesses: React.FC<Props> = ({
     return `${state.node_id} - ${status} - ${createdAt}`;
   }
 
+  const onChangeProcesses = (
+    value: string,
+    campo: "Status" | "NodeId" | "CreatedAt"
+  ) => {
+    setProcesses((prev) => ({ ...prev, [campo]: value }));
+  };
+
   useEffect(() => {
     const request = async () => {
       const response = await listByWorkflowId(workflowId);
@@ -56,12 +64,53 @@ export const ListProcesses: React.FC<Props> = ({
     }
   }, [isOpen, workflowId]);
 
+  const [value, setValue] = React.useState<Date | any>(
+    new Date("2014-08-18T21:11:54")
+  );
+
+  const handleChange = (newValue: Date | any) => {
+    setValue(newValue);
+  };
+
   return (
     <S.Wrapper open={isOpen} onClose={onClose}>
       <S.Title>
         Lista de processos
         <S.CloseButton onClick={onClose} />
       </S.Title>
+
+      <S.InputContainer>
+        <S.InputProcess
+          label="node id"
+          value={processes}
+          onChange={(event) => {
+            onChangeProcesses(event.target.value, "NodeId");
+          }}
+        >
+          node_id
+        </S.InputProcess>
+        <S.InputProcess
+          label="status"
+          value={processes}
+          onChange={(event) => {
+            onChangeProcesses(event.target.value, "Status");
+          }}
+        >
+          status
+        </S.InputProcess>
+      </S.InputContainer>
+
+      <S.Provider dateAdapter={AdapterDateFns}>
+        <S.StackDate spacing={3}>
+          <S.DatePicker
+            label="Date desktop"
+            inputFormat="MM/dd/yyyy"
+            value={value}
+            onChange={handleChange}
+            renderInput={(params: any) => <S.InputDate {...params} />}
+          />
+        </S.StackDate>
+      </S.Provider>
 
       <S.Content dividers>
         <List>
--- src/pages/diagram/dialogs/list-processes/styles.ts
+++ src/pages/diagram/dialogs/list-processes/styles.ts
@@ -7,8 +7,11 @@ import DialogTitle from "@mui/material/DialogTitle";
 
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 import CloseIcon from "@mui/icons-material/Close";
-
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { StaticDatePicker } from "@mui/x-date-pickers/StaticDatePicker";
+import Stack from "@mui/material/Stack";
 import { IconButton } from "shared/components/icon-button";
+import TextField from "@mui/material/TextField";
 
 export const Wrapper = styled(Dialog).attrs({
   "aria-labelledby": "List process dialog",
@@ -18,11 +21,26 @@ export const Wrapper = styled(Dialog).attrs({
   maxWidth: "md",
 })``;
 
+export const InputContainer = styled("div")``;
+
+export const Provider = styled(LocalizationProvider)``;
+export const StackDate = styled(Stack)``;
+export const DatePicker = styled(StaticDatePicker)``;
+export const InputDate = styled(TextField)``;
 export const Title = styled(DialogTitle)`
   display: flex;
   align-items: center;
   justify-content: space-between;
 `;
+
+export const InputProcess = styled(TextField).attrs({
+  variant: "outlined",
+  sx: {
+    m: 2,
+    display: "inline-block",
+    width: "25ch",
+  },
+})``;
 
 export const CloseButton = styled(IconButton).attrs({
   icon: CloseIcon,
