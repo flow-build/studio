b39dee7ab93ae48f0a8f5ff7bcd1f19fc9e67c1d
Gabriela Sales <gabriela.coutinho.sales@gmail.com>
2022-08-23 12:00:41 -0300

    added modal component to cards and functions

--- src/pages/workflows/components/cards-view/cards-view.tsx
+++ src/pages/workflows/components/cards-view/cards-view.tsx
@@ -1,45 +1,52 @@
-import AddOutlined from '@mui/icons-material/AddOutlined'
-import ExtensionOutlined from '@mui/icons-material/ExtensionOutlined'
-import VisibilityOutlined from '@mui/icons-material/VisibilityOutlined'
+import AddOutlined from "@mui/icons-material/AddOutlined";
+import ExtensionOutlined from "@mui/icons-material/ExtensionOutlined";
+import VisibilityOutlined from "@mui/icons-material/VisibilityOutlined";
 
-import { TWorkflow } from 'models/workflow'
+import { TWorkflow } from "models/workflow";
 
-import { useWorkflowPage } from 'pages/workflows/hooks/useWorkflowPage'
+import { useWorkflowPage } from "pages/workflows/hooks/useWorkflowPage";
+import { useState } from "react";
 
-import { getLongFormatByDate } from 'shared/utils/date'
+import { getLongFormatByDate } from "shared/utils/date";
+import { Modal } from "../modal";
 
-import * as S from './styles'
+import * as S from "./styles";
 
 type Props = {
-  workflows: TWorkflow[]
-}
+  workflows: TWorkflow[];
+};
 
 export const CardsView: React.FC<Props> = ({ workflows }) => {
-  const workflowPage = useWorkflowPage()
+  const workflowPage = useWorkflowPage();
+  const [openDialog, setOpenDialog] = useState(false);
 
   const getActions = (workflow: TWorkflow) => {
     return [
       {
         icon: VisibilityOutlined,
-        tooltip: 'Ver processos',
-        onClick: () => workflowPage.navigateToProcess(workflow.workflow_id)
+        tooltip: "Ver processos",
+        onClick: () => workflowPage.navigateToProcess(workflow.workflow_id),
       },
       {
         icon: AddOutlined,
-        tooltip: 'Novo processo',
-        onClick: () => workflowPage.createProcess(workflow.name, workflow.workflow_id)
+        tooltip: "Novo processo",
+        onClick: () => setOpenDialog(true),
       },
       {
         icon: ExtensionOutlined,
-        tooltip: 'Ver diagrama',
-        onClick: () => workflowPage.navigateToDiagram(workflow.workflow_id)
-      }
+        tooltip: "Ver diagrama",
+        onClick: () => workflowPage.navigateToDiagram(workflow.workflow_id),
+      },
     ];
-  }
+  };
+
+  const handleDialogClose = () => {
+    setOpenDialog(false);
+  };
 
   return (
     <>
-      {workflows.map(workflow => (
+      {workflows.map((workflow) => (
         <S.Cards
           key={workflow.workflow_id}
           title={workflow.name}
@@ -50,6 +57,14 @@ export const CardsView: React.FC<Props> = ({ workflows }) => {
           actions={getActions(workflow)}
         />
       ))}
+
+      <Modal
+        onClose={handleDialogClose}
+        open={openDialog}
+        onClick={() =>
+          workflowPage.createProcess(workflow.name, workflow.workflow_id)
+        }
+      />
     </>
-  )
-}
+  );
+};
