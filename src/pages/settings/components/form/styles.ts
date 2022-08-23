a7e9018caa0efc59eca61f12048aee59925937f6
Dayan <dayan.monteiro@fdte.io>
2022-08-23 09:12:08 -0300

    form styling

--- src/pages/settings/components/form/styles.ts
+++ src/pages/settings/components/form/styles.ts
@@ -9,12 +9,16 @@ export const Wrapper = styled.div`
   display: flex;
   align-items: center;
   column-gap: 15px;
+  margin-bottom: 1rem;
 `;
 
 export const Input = styled(InputText)<{ small?: boolean }>`
   width: ${({ small }) => (small ? 250 : 550)}px;
 `;
 
-export const Button = styled(BaseButton)``;
+export const Button = styled(BaseButton)`
+  width: 6rem;
+  height: 3.5rem;
+`;
 
 export const StatusConnection = styled(BaseStatusConnection)``;
--- src/pages/settings/styles.ts
+++ src/pages/settings/styles.ts
@@ -1,7 +1,4 @@
 import styled from "styled-components";
-
-// import { FormServerMqtt } from "pages/settings/components/form-server-mqtt";
-// import { FormServerFlowbuild } from "pages/settings/components/form-server-flowbuild";
 import { Form as BaseForm } from "pages/settings/components/form";
 import { Button as BaseButton } from "shared/components/button";
 
@@ -12,10 +9,9 @@ export const Title = styled.h1`
   margin: 2rem 1rem;
 `;
 
-// export const FormMqtt = styled(FormServerMqtt)``;
-
-// export const FormServer = styled(FormServerFlowbuild)``;
-
 export const Form = styled(BaseForm)``;
 
-export const Button = styled(BaseButton)``;
+export const Button = styled(BaseButton)`
+  width: 6rem;
+  height: 3.5rem;
+`;
