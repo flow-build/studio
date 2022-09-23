a22a3ee43b4801d664dc6fd490e1c4b7332eb470
Gabriela Sales <gabriela.coutinho.sales@gmail.com>
2022-09-23 16:58:23 -0300

    [feat]: get info

--- src/pages/diagram/dialogs/save-diagram/save-diagram.tsx
+++ src/pages/diagram/dialogs/save-diagram/save-diagram.tsx
@@ -4,29 +4,27 @@ import { useParams } from "react-router-dom";
 import { getStorageItem } from "shared/utils/storage";
 import { IPayload } from "pages/diagram/dialogs/save-diagram/types/IPayload";
 import { create } from "services/resources/diagrams/create";
-import { useDiagram } from "pages/diagram/hooks/useDiagram";
 
 import { useSnackbar } from "notistack";
-
 import jwt_decode from "jwt-decode";
 
 import * as S from "./styles";
+import { TUser } from "models/user";
 
 type Props = {
   isOpen: boolean;
   onClose?: () => void;
   onClick?: React.MouseEventHandler<HTMLButtonElement>;
+  xml: any;
 };
 
-export const SaveDiagram: React.FC<Props> = ({ isOpen, onClose }) => {
+export const SaveDiagram: React.FC<Props> = ({ isOpen, onClose, xml }) => {
   const { enqueueSnackbar } = useSnackbar();
-  const diagram = useDiagram();
   const { workflowId } = useParams();
 
   function getUserInfo() {
     const token = getStorageItem("TOKEN");
-    const decoded = jwt_decode(token);
-    console.log(decoded, "decoded");
+    const decoded = jwt_decode(token) as TUser;
     return decoded;
   }
 
@@ -46,49 +44,49 @@ export const SaveDiagram: React.FC<Props> = ({ isOpen, onClose }) => {
   }
 
   async function handleClickDiagramName() {
-    console.log(payload);
     const info = getUserInfo();
-    console.log(info);
+
     const diagramName = payload?.name;
 
     const response = await create({
       name: payload.name,
       workflowId: workflowId as string,
       userId: info.actor_id,
-      xml: diagram,
+      xml: xml,
     });
     createDiagramSuccess(diagramName);
-    console.log(response, "response");
 
     return;
   }
 
   return (
     <S.DiagramWrapper open={isOpen} onClose={onClose}>
-      <S.DiagramTitle>
-        Salvar Diagrama
-        <S.CloseDiagramButton onClick={onClose} />
-      </S.DiagramTitle>
-      <S.DiagramContent>
-        <S.DiagramInput
-          value={payload?.name}
-          onChange={(event) => {
-            onChangeDiagramName(event.target.value, "name");
-          }}
-        />
-      </S.DiagramContent>
-      <S.DiagramDivider />
-      <S.ButtonWrapper>
-        <S.CancelDiagramButton onClick={onClose}>
-          {" "}
-          Cancelar{" "}
-        </S.CancelDiagramButton>
-        <S.ButtonDivider />
-        <S.SaveDiagramButton onClick={handleClickDiagramName}>
-          {" "}
-          Salvar
-        </S.SaveDiagramButton>
-      </S.ButtonWrapper>
+      <S.DiagramBackground>
+        <S.DiagramTitle>
+          Salvar Diagrama
+          <S.CloseDiagramButton onClick={onClose} />
+        </S.DiagramTitle>
+        <S.DiagramContent>
+          <S.DiagramInput
+            value={payload?.name}
+            onChange={(event) => {
+              onChangeDiagramName(event.target.value, "name");
+            }}
+          />
+        </S.DiagramContent>
+        <S.DiagramDivider />
+        <S.ButtonWrapper>
+          <S.CancelDiagramButton onClick={onClose}>
+            {" "}
+            Cancelar{" "}
+          </S.CancelDiagramButton>
+          <S.ButtonDivider />
+          <S.SaveDiagramButton onClick={handleClickDiagramName}>
+            {" "}
+            Salvar
+          </S.SaveDiagramButton>
+        </S.ButtonWrapper>
+      </S.DiagramBackground>
     </S.DiagramWrapper>
   );
 };
