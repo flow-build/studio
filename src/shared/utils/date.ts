c2e3c6fe1c84309bfa83db309d95e068ed293e8e
Gabriela Sales <gabriela.coutinho.sales@gmail.com>
2022-09-28 11:53:13 -0300

    feat: add short format date function

--- src/shared/utils/date.ts
+++ src/shared/utils/date.ts
@@ -1,4 +1,5 @@
 import { format } from "date-fns";
+import ptBRLocale from "date-fns/locale/pt-BR";
 
 export function getDateTimeFormatByDate(text: string) {
   try {
@@ -14,6 +15,16 @@ export function getLongFormatByDate(text: string) {
   try {
     const date = new Date(text);
     return new Intl.DateTimeFormat("pt-BR", { dateStyle: "full" }).format(date);
+  } catch (error) {
+    throw new Error("Erro ao retornar data formatada");
+  }
+}
+
+export function getShortFormatByDate(text: string) {
+  try {
+    const date = new Date(text);
+    const maskDate = format(date, "dd/MMM/yyyy", { locale: ptBRLocale });
+    return maskDate;
   } catch (error) {
     throw new Error("Erro ao retornar data formatada");
   }
