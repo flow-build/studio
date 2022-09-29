79373325126889e46426f59d740f68f0cb041736
Gabriela Sales <gabriela.coutinho.sales@gmail.com>
2022-09-29 18:40:05 -0300

    feat: moved folder

---
+++ src/dialogs/list-diagrams/list-diagrams.tsx
@@ -0,0 +1,79 @@
+import { useEffect, useState } from "react";
+
+import { TUser } from "models/user";
+import { listById } from "services/resources/diagrams/list-by-id";
+
+import { getShortFormatByDate } from "shared/utils/date";
+
+import * as S from "./styles";
+import { setDiagramSelected } from "store/slices/dialog";
+import { useDispatch } from "react-redux";
+
+type Props = {
+  isOpen: boolean;
+  onClose?: () => void;
+  id: string;
+  onSelectDiagram?: (diagram: TUser) => void;
+};
+
+export const ListDiagrams: React.FC<Props> = ({
+  isOpen,
+  onClose,
+  id,
+  onSelectDiagram,
+}) => {
+  const dispatch = useDispatch();
+  const [listDiagram, setListDiagram] = useState<TUser[]>([]);
+
+  function onClickListDiagram(diagram: TUser) {
+    if (onClose) {
+      onClose();
+    }
+    if (onSelectDiagram) {
+      onSelectDiagram(diagram);
+    }
+  }
+
+  function getSubtitle(diagram: TUser) {
+    const createdAt = getShortFormatByDate(diagram.created_at);
+    const updatedAt = getShortFormatByDate(diagram.updated_at);
+    return `criado em: ${createdAt} - atualizado em: ${updatedAt}`;
+  }
+
+  useEffect(() => {
+    const request = async () => {
+      const response = await listById(id);
+      console.log(response, "diagrams");
+
+      setListDiagram(response);
+    };
+    if (isOpen) {
+      request();
+    }
+  }, [isOpen, id]);
+
+  return (
+    <S.ListDiagramsWrapper open={isOpen} onClose={onClose}>
+      <S.ListTitle>
+        Lista de diagramas
+        <S.CloseListButton onClick={onClose} />
+      </S.ListTitle>
+
+      <S.Content dividers>
+        <S.ListDiagram>
+          {listDiagram.map((diagram) => (
+            <S.ItemDiagram disablePadding>
+              <S.ItemButton onClick={() => onClickListDiagram(diagram)}>
+                <S.TextDiagram
+                  primary={diagram.name}
+                  secondary={getSubtitle(diagram)}
+                />
+                <S.RightArrowList />
+              </S.ItemButton>
+            </S.ItemDiagram>
+          ))}
+        </S.ListDiagram>
+      </S.Content>
+    </S.ListDiagramsWrapper>
+  );
+};
