55592d03fcc979d8d9338a1a34e5a7f32810b02d
Gabriela Sales <gabriela.coutinho.sales@gmail.com>
2022-10-07 12:25:02 -0300

    feat: get data and deleted console

--- src/dialogs/list-diagrams/list-diagrams.tsx
+++ src/dialogs/list-diagrams/list-diagrams.tsx
@@ -1,48 +1,27 @@
-import { useEffect, useState, useCallback } from "react";
-
 import { TUser } from "models/user";
-import { listByWorkflow } from "services/resources/diagrams/list-by-workflow";
+
+import { useSelector } from "react-redux";
+import { RootState } from "store";
 
 import { getShortFormatByDate } from "shared/utils/date";
-import _isEmpty from "lodash/isEmpty";
 
 import * as S from "./styles";
-import { setDiagramSelected } from "store/slices/dialog";
-import { useDispatch, useSelector } from "react-redux";
-import { RootState } from "store";
 
 type Props = {
   isOpen: boolean;
   onClose?: () => void;
-  // id: string;
   onSelectDiagram?: (diagram: TUser) => void;
 };
 
 export const ListDiagrams: React.FC<Props> = ({
   isOpen,
   onClose,
-  // id,
+
   onSelectDiagram,
 }) => {
-  //const [listDiagram, setListDiagram] = useState<TUser[]>([]);
-  // console.log(listDiagram, "listDiagram");
-
-  const dispatch = useDispatch();
-
-  const data:TUser[] = useSelector(
+  const data = useSelector(
     (state: RootState) => state.dialogPage.diagramInfoDialog.data
   );
-  console.log(data, "data");
-
-  // function getResponse() {
-  //   const response = data;
-  //   return response;
-  // }
-
-  // useEffect(() => {
-  //   if (isOpen) {
-  //   }
-  // });
 
   function onClickListDiagram(diagram: TUser) {
     if (onClose) {
@@ -68,18 +47,19 @@ export const ListDiagrams: React.FC<Props> = ({
 
       <S.Content dividers>
         <S.ListDiagram>
-          {data.map((diagram: any) => (
-            <S.ItemDiagram disablePadding>
-              <S.ItemButton onClick={() => onClickListDiagram(diagram)}>
-                <S.TextDiagram
-                  key={diagram.id}
-                  primary={diagram.name}
-                  secondary={getSubtitle(diagram)}
-                />
-                <S.RightArrowList />
-              </S.ItemButton>
-            </S.ItemDiagram>
-          ))}
+          {data.length > 0 &&
+            data?.map((diagram: any) => (
+              <S.ItemDiagram disablePadding>
+                <S.ItemButton onClick={() => onClickListDiagram(diagram)}>
+                  <S.TextDiagram
+                    key={diagram.id}
+                    primary={diagram.name}
+                    secondary={getSubtitle(diagram)}
+                  />
+                  <S.RightArrowList />
+                </S.ItemButton>
+              </S.ItemDiagram>
+            ))}
         </S.ListDiagram>
       </S.Content>
     </S.ListDiagramsWrapper>
