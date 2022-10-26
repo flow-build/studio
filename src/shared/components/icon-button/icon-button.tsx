316e66f5c9b7cbb1171b8f7a9fda0d17731f7d5e
Gabriela Sales <gabriela.coutinho.sales@gmail.com>
2022-10-26 19:25:06 -0300

    feat: badge logic

--- src/shared/components/icon-button/icon-button.tsx
+++ src/shared/components/icon-button/icon-button.tsx
@@ -1,8 +1,6 @@
-import { BadgeTypeMap, SvgIconTypeMap } from "@mui/material";
+import {SvgIconTypeMap } from "@mui/material";
 import { OverridableComponent } from "@mui/material/OverridableComponent";
 import React from "react";
-
-// import { VisibilityOutlined, ExtensionOutlined, AddOutlined, ViewList, ViewModule } from '@mui/icons-material';
 
 import * as S from "./styles";
 
@@ -10,20 +8,25 @@ type Props = {
   tooltip?: string;
   onClick?: React.MouseEventHandler<HTMLButtonElement> | undefined;
   icon: OverridableComponent<SvgIconTypeMap<{}, "svg">>;
-  badge?: OverridableComponent<BadgeTypeMap<"span", {}>>;
+  badge?: number;
 };
 
 export const IconButton: React.FC<Props> = ({
   icon: Icon,
   tooltip = "",
+  badge,
   ...props
 }) => {
   return (
     <S.Wrapper title={tooltip}>
       <S.Button {...props}>
-        <S.Notification badgeContent={0} variant="dot">
-          <Icon />
-        </S.Notification>
+        {badge && (
+          <S.Notification badgeContent={badge} >
+            <Icon />
+          </S.Notification>
+        )}
+
+        {!badge && <Icon />}
       </S.Button>
     </S.Wrapper>
   );
