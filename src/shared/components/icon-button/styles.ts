0ab2d20623f5f3cc1cc701341d5cfbbacdaa5f5c
Gabriela Sales <gabriela.coutinho.sales@gmail.com>
2022-10-26 19:25:16 -0300

    feat: badge styles

--- src/shared/components/icon-button/styles.ts
+++ src/shared/components/icon-button/styles.ts
@@ -1,11 +1,13 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
-import IconButton from '@mui/material/IconButton'
-import Tooltip from '@mui/material/Tooltip'
-import Badge from '@mui/material/Badge';
+import IconButton from "@mui/material/IconButton";
+import Tooltip from "@mui/material/Tooltip";
+import Badge from "@mui/material/Badge";
 
-export const Wrapper = styled(Tooltip)``
+export const Wrapper = styled(Tooltip)``;
 
-export const Button = styled(IconButton)``
+export const Button = styled(IconButton)``;
 
-export const Notification = styled(Badge)``;
+export const Notification = styled(Badge).attrs({
+  color: "primary",
+})``;
