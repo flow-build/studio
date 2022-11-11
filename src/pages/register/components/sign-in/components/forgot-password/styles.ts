b98e96419502b499fefeedf12b57ff3745fdfc4b
Michele Borowski <michele.borowski@fdte.io>
2022-11-11 17:22:02 -0300

    refactor: add buttons

--- src/pages/register/components/sign-in/components/forgot-password/styles.ts
+++ src/pages/register/components/sign-in/components/forgot-password/styles.ts
@@ -15,9 +15,35 @@ export const Input = styled(InputText).attrs({
   sx: { mb: 2 },
 })``;
 
+export const SubmitContainer = styled.div`
+  display: flex;
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  gap: 5px;
+
+  margin: 20px 0 20px;
+`;
+
 export const SubmitButton = styled(Button).attrs({
   fullWidth: true,
   type: "submit",
+  variant: "contained",
+})`
+  height: 3.5rem;
+`;
+
+export const BackButton = styled(Button).attrs({
+  type: "button",
+  title: "Back",
+  variant: "outlined",
+  fullWidth: true,
+})`
+  height: 3.5rem;
+`;
+
+export const ResendCodeButton = styled(Button).attrs({
+  fullWidth: true,
   variant: "contained",
 })`
   height: 3.5rem;
