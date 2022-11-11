a379baeef14e023f7cb7ebbb3a5b41b1a03ad4e2
Michele Borowski <michele.borowski@fdte.io>
2022-11-11 17:23:19 -0300

    feat: add button resend code

--- src/pages/register/components/sign-up/components/form-confirmation-code/form-confirmation-code.tsx
+++ src/pages/register/components/sign-up/components/form-confirmation-code/form-confirmation-code.tsx
@@ -1,13 +1,15 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { Auth } from "aws-amplify";
 
 import _isEmpty from "lodash/isEmpty";
 
 import * as S from "./styles";
 
-type Props = {signedUp: boolean};
+type Props = { signedUp: boolean };
 
-export const FormConfirmationCode: React.FC<Props> = ({signedUp}) => {
+export const FormConfirmationCode: React.FC<Props> = ({ signedUp }) => {
+  const navigate = useNavigate();
   const [state, setState] = useState({
     confirmed: false,
     confirmationCode: "",
@@ -41,6 +43,7 @@ export const FormConfirmationCode: React.FC<Props> = ({signedUp}) => {
           submittingConfirmation: false,
           confirmed: true,
         }));
+        navigate("./dashboard");
       }
     } catch (error) {
       setState((prev) => ({ ...prev, submittingConfirmation: false }));
@@ -64,7 +67,13 @@ export const FormConfirmationCode: React.FC<Props> = ({signedUp}) => {
         placeholder="Type your confirmation code"
         onChange={handleChange}
       />
-      <S.SubmitButton title={"Submit"} disabled={disabled}></S.SubmitButton>
+      <S.SubmitContainer>
+        <S.SubmitButton title={"Submit"} disabled={disabled}/>
+        <S.ResendCodeButton
+          title={"Resend code"}
+          onClick={() => Auth.resendSignUp(state.email)}
+        />
+      </S.SubmitContainer>
     </S.Form>
   );
 };
--- src/pages/register/components/sign-up/components/form-confirmation-code/styles.ts
+++ src/pages/register/components/sign-up/components/form-confirmation-code/styles.ts
@@ -6,19 +6,36 @@ import { Button } from "shared/components/button";
 import { InputText } from "shared/components/input-text";
 
 export const Form = styled(Box).attrs({
-    component: "form",
-  })``;
-  
-export const Input = styled(InputText).attrs({
-    fullWidth: true,
-    required: true,
-    sx: { mb: 2 },
-  })``;
+  component: "form",
+})``;
 
-  export const SubmitButton = styled(Button).attrs({
-    type: "submit",
-    variant: "contained",
-    fullWidth: true,
-  })`
+export const Input = styled(InputText).attrs({
+  fullWidth: true,
+  required: true,
+  sx: { mb: 2 },
+})``;
+
+export const SubmitButton = styled(Button).attrs({
+  type: "submit",
+  variant: "contained",
+  fullWidth: true,
+})`
     height: 3.5rem;
   `;
+
+export const ResendCodeButton = styled(Button).attrs({
+  variant: "contained",
+  fullWidth: true,
+})`
+height: 3.5rem;
+`;
+
+export const SubmitContainer = styled.div`
+  display: flex;
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  gap: 5px;
+
+  margin: 40px 0 20px;
+`;
