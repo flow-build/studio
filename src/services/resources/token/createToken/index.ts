bed254843050abfd75ec22ff15f0fc676fb796d0
Michele Borowski <michele.borowski@fdte.io>
2022-12-02 10:43:16 -0300

    refactor: change function name

---
+++ src/services/resources/token/createToken/index.ts
@@ -0,0 +1,11 @@
+import { api } from "services/api";
+
+export const createToken = async (user_id: string) => {
+  try {
+    const response = await api.post("/token", { user_id }, { headers: { 'x-duration': 1 } });
+    return response.data.jwtToken || response.data.token;
+  } catch (error: any) {
+    throw new Error(`getWorkflowAnonymousToken -> ${error.error}: ${error.message}`);
+  }
+};
+
