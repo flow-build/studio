9be21d94de8f589661353e5de511c53444efdf42
Gabriela Sales <gabriela.coutinho.sales@gmail.com>
2022-12-13 18:36:17 -0300

    feat: delete unecessary code

--- src/pages/metabase/metabase.tsx
+++ src/pages/metabase/metabase.tsx
@@ -1,4 +1,4 @@
-import sign from "jwt-encode";
+import { getStorageItem } from "shared/utils/storage";
 
 import * as S from "./styles";
 
@@ -6,19 +6,8 @@ export const Metabase: React.FC = () => {
   const iframeUrl = getIframURL();
 
   function getIframURL() {
-    const METABASE_SITE_URL = process.env.REACT_APP_METABASE_SITE_URL;
-    const METABASE_SECRET_KEY = process.env.REACT_APP_METABASE_SECRET_KEY as string;
-    const METABASE_DASHBOARD_NUMBER = process.env.REACT_APP_METABASE_DASHBOARD_NUMBER;
-
-    const payload = {
-      resource: { dashboard: Number(METABASE_DASHBOARD_NUMBER)},
-      params: {},
-      exp: Math.round(Date.now() / 1000) + (10 * 60), // 10 minute expiration
-    };
-
-    const token = sign(payload, METABASE_SECRET_KEY);
-
-    return `${METABASE_SITE_URL}/embed/dashboard/${token}#theme=night&bordered=true&titled=true`;
+    const metabaseUrlConfig = getStorageItem("DASHBOARD");
+    return `${metabaseUrlConfig}/embed/dashboard/${metabaseUrlConfig}#theme=night&bordered=true&titled=true`
   }
 
   return (
