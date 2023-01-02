286ca74b1789c5bdee33f3b44c3994b21c57ed45
Gabriela Sales <gabriela.coutinho.sales@gmail.com>
2023-01-02 13:17:01 -0300

    fix: rewrite function iframe

--- src/pages/metabase/metabase.tsx
+++ src/pages/metabase/metabase.tsx
@@ -1,14 +1,14 @@
+import { useMemo } from "react";
 import { getStorageItem } from "shared/utils/storage";
 
 import * as S from "./styles";
 
 export const Metabase: React.FC = () => {
-  const iframeUrl = getIframURL();
-
-  function getIframURL() {
+  const iframeUrl = useMemo(() => {
     const metabaseUrlConfig = getStorageItem("DASHBOARD");
+
     return `${metabaseUrlConfig}/embed/dashboard/${metabaseUrlConfig}#theme=night&bordered=true&titled=true`
-  }
+}, []);
 
   return (
     <S.Wrapper>
