b85244f3206ddea4e99cd5dea581f9be1792e8fd
elsonfojr-fdte <elson.oliveira@fdte.io>
2023-05-11 17:57:10 -0300

    [FBS-133] - feat: hide header content instead of component

--- src/pages/diagram/components/header/styles.ts
+++ src/pages/diagram/components/header/styles.ts
@@ -4,11 +4,7 @@ import Box from "@mui/material/Box";
 import MuiTooltip from "@mui/material/Tooltip";
 import Typography from "@mui/material/Typography";
 
-export const Wrapper = styled(Box).attrs({
-  sx: {
-    background: (theme) => theme.palette.background.paper,
-  },
-})`
+export const Wrapper = styled(Box)<{ noPadding: boolean }>`
   margin: 16px 9px 9px 31px;
   height: 48px;
   display: flex;
@@ -16,6 +12,9 @@ export const Wrapper = styled(Box).attrs({
   flex: 0.7;
   padding: 5px;
   border-radius: 15px;
+  background: ${({ theme }) => theme.palette.background.paper};
+
+  ${({ noPadding }) => noPadding && { padding: 0 }}
 `;
 
 export const TitleContent = styled.div`
@@ -40,4 +39,3 @@ export const Status = styled(Box).attrs({})`
   opacity: 70%;
   margin-right: 24px;
 `;
-
