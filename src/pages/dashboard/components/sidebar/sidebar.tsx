07adc09d39f0933a8bc842b2ad8b52cd2876440c
elsonfojr-fdte <elson.oliveira@fdte.io>
2023-05-22 10:57:08 -0300

    [FS-55] - refactor: put finally on promise function

--- src/pages/dashboard/components/sidebar/sidebar.tsx
+++ src/pages/dashboard/components/sidebar/sidebar.tsx
@@ -39,7 +39,7 @@ export const Sidebar: React.FC<Props> = ({ isOpen }) => {
       tryingConnection.current = false;
     };
 
-    connect();
+    connect().finally();
   }, [connectToMqtt]);
 
   return (
