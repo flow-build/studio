5d6b86e8d1adaf5758ebb014c437efbcf58b0131
elsonfojr-fdte <elson.oliveira@fdte.io>
2023-05-23 00:48:13 -0300

    [FS-60] - fix: adjust height for content

--- src/pages/history/styles.ts
+++ src/pages/history/styles.ts
@@ -3,14 +3,14 @@ import styled from "styled-components";
 import Grid from "@mui/material/Grid";
 
 import { Table as SharedTable } from "shared/components/table";
-import { Button as SharedButton } from 'shared/components/button'
+import { Button as SharedButton } from "shared/components/button";
 
 export const Wrapper = styled(Grid).attrs({
   container: true,
   spacing: 2,
   sx: { marginTop: 0 },
 })`
-  height: calc(100% - 64px);
+  height: calc(100% - 64px - 30px);
 `;
 
 export const HeaderContainer = styled.div`
