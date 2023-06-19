3d2b4d67dc778a39895a5dcfdc86ad1c5e88e2b7
elsonfojr-fdte <elson.oliveira@fdte.io>
2023-06-19 17:03:29 -0300

    [FS-224] - chore: sonarcloud adjust for email validation

--- src/utils/validators/email/index.ts
+++ src/utils/validators/email/index.ts
@@ -1,4 +1,5 @@
 export function validateEmail(email: string): boolean {
-  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/;
-  return emailRegex.test(email);
+  const regex =
+    /^(?=.{1,64}@)[A-Za-z0-9_-]+(\.[A-Za-z0-9_-]+)*@[A-Za-z0-9_-]+(\.[A-Za-z0-9_-]+)*(\.[A-Za-z]{2,})$/;
+  return regex.test(email);
 }
