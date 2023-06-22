2ec9bd483f5a31c4fde23bc81428b17750cff5c5
elsonfojr-fdte <elson.oliveira@fdte.io>
2023-06-21 22:36:18 -0300

    [FS-230] - feat: grid cards added to processes page

--- src/pages/workflows/[workflowId]/processes/index.tsx
+++ src/pages/workflows/[workflowId]/processes/index.tsx
@@ -1,7 +1,9 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import { DataGrid } from '@mui/x-data-grid';
 import { ContentHeader } from 'components/ContentHeader';
+import { MiniCardsGrid } from 'components/MiniCardsGrid';
+import format from 'date-fns/format';
 import { useProcessesPage } from 'hooks/pages/processes';
 import flowbuildApi from 'services/flowbuildServer';
 import { ModeView } from 'shared/enum';
@@ -20,9 +22,36 @@ export const getServerSideProps: ServerSideProcessesPageProps = async ({ req, pa
 export default function Process({ processes }: ProcessPageProps) {
   const { columns, rowData } = useProcessesPage(processes);
   const [modeView, setModeView] = useState<ModeView>(ModeView.TABLE);
+  const [cards, setCards] = useState<any[]>([]);
+  const totalPage = Math.ceil(processes.length / 9);
 
   const isTableModeView = modeView === ModeView.TABLE;
   const isCardModeView = modeView === ModeView.CARDS;
+
+  function onChangePage(page: number) {
+    const slicedProcesses = processes.slice((page - 1) * 9, (page - 1) * 9 + 9).map((process) => ({
+      name: process.state.node_name,
+      description: process.id,
+      text: `${process.status} | ${format(new Date(process.created_at), 'dd-MM-yyyy')}`
+    }));
+
+    setCards(slicedProcesses);
+  }
+
+  useEffect(() => {
+    if (!isCardModeView) {
+      setCards([]);
+      return;
+    }
+
+    const slicedProcesses = processes.slice(0, 9).map((process) => ({
+      name: process.state.node_name,
+      description: process.id,
+      text: `${process.status} | ${format(new Date(process.created_at), 'dd-MM-yyyy')}`
+    }));
+
+    setCards(slicedProcesses);
+  }, [isCardModeView, processes]);
 
   return (
     <>
@@ -43,7 +72,9 @@ export default function Process({ processes }: ProcessPageProps) {
         />
       )}
 
-      {isCardModeView && <h1>card</h1>}
+      {isCardModeView && (
+        <MiniCardsGrid items={cards} totalPage={totalPage} onChangePage={onChangePage} />
+      )}
     </>
   );
 }
