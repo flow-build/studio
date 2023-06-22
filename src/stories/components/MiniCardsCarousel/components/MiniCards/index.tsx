fe3b7ea2a742056c9f1e44b6ea1c71e1639356f1
elsonfojr-fdte <elson.oliveira@fdte.io>
2023-06-21 22:34:59 -0300

    [FS-230] - feat: change urlImg from required to optional and adjusting link for minicard

--- src/stories/components/MiniCardsCarousel/components/MiniCards/index.tsx
+++ src/stories/components/MiniCardsCarousel/components/MiniCards/index.tsx
@@ -1,19 +1,31 @@
+import { Fragment } from 'react';
+
 import Link from 'next/link';
 
 import * as S from './styles';
 import { MiniCardsProps } from './types';
 
-export function MiniCards({ name, urlRedirect, description, urlImg, text }: MiniCardsProps) {
+export function MiniCards({
+  name,
+  urlRedirect,
+  description,
+  urlImg,
+  text,
+  ...props
+}: MiniCardsProps) {
+  const Container = urlRedirect ? Link : Fragment;
+
   return (
-    <Link href={urlRedirect ?? ''}>
-      <S.Wrapper>
-        <S.Img alt={name} src={urlImg ?? ''} width={155} height={100} />
+    <Container href={urlRedirect ?? ''}>
+      <S.Wrapper {...props}>
+        {urlImg && <S.Img alt={name} src={urlImg ?? ''} width={155} height={100} />}
+
         <S.WrapperContent>
           <S.Name>{name}</S.Name>
           <S.Description>{description}</S.Description>
           <S.ContentText>{text}</S.ContentText>
         </S.WrapperContent>
       </S.Wrapper>
-    </Link>
+    </Container>
   );
 }
