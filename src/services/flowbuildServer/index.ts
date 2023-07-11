23a653cda09aff439e0b6e7c7e493d2fc77424f6
Gustavo Haramura <gustavo.haramura@fdte.io>
2023-07-11 17:14:42 -0300

    ci: adjust ci env step

--- src/services/flowbuildServer/index.ts
+++ src/services/flowbuildServer/index.ts
@@ -2,6 +2,7 @@ import { apiConfig } from 'config/api';
 import { default as api } from 'services/httpClient';
 
 const flowbuildApi = api();
+console.log('BaseURL: ', apiConfig.baseUrl)
 
 flowbuildApi.setBaseUrl(apiConfig.baseUrl ?? '');
 
